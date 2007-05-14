1c1a3e02af7487c45f4160223175f7b898bfe28f
droptchyald <droptchyald@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2007-05-14 03:00:25 +0000

    Initial debian package stuff.  Still needs to be configured.

---
+++ dmtcp/debian/dmtcp-default.ex
@@ -0,0 +1,10 @@
+# Defaults for dmtcp initscript
+# sourced by /etc/init.d/dmtcp
+# installed at /etc/default/dmtcp by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ dmtcp/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(dmtcp):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="dmtcp" command="/usr/bin/dmtcp"
