6a3c3d1fd275080f7e1ab33aa2165cfc06a3389e
mrieker <mrieker@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2007-09-25 00:07:08 +0000

    Fix to run on glibc 2.6.1:   * reset signal handler in threads each checkpoint cycle (setup_sig_handler)   * checkpoint thread re-created with clone like the other threads and set     up special state (ST_CKPNTHREAD) to separate it out from the others   * testmtcp2.c: bracket printf/fflush with mutex esle it accvio's   * mtcp_printf.c: bracket output with mutex so output doesn't get scrambled   * Makefile: add mtcp_state.o wherever mtcp_printf.o is used   * mtcp_state.c: fix futex routine   * mtcp_futex.h: provide futex syscall primitive

---
+++ trunk/mtcp/mtcp_futex.h
@@ -0,0 +1,36 @@
+#ifndef _MTCP_FUTEX_H
+#define _MTCP_FUTEX_H
+
+/* Glibc does not provide a futex routine, so provide one here... */
+
+static inline int mtcp_futex (int *uaddr, int op, int val, const struct timespec *timeout)
+
+{
+#if defined(__x86_64__)
+  int rc;
+
+  register long int a1 asm ("rdi") = (long int)uaddr;
+  register long int a2 asm ("rsi") = (long int)op;
+  register long int a3 asm ("rdx") = (long int)val;
+  register long int a4 asm ("r10") = (long int)timeout;
+  register long int a5 asm ("r8")  = (long int)0;
+
+  asm volatile ("syscall"
+                : "=a" (rc)
+                : "0" (__NR_futex), 
+                  "r" (a1), "r" (a2), "r" (a3), "r" (a4), "r" (a5)
+                : "memory", "cc", "r11", "cx");
+
+  return (rc);
+#elif defined(__i386__)
+  asm volatile ("int $0x80"
+                : "=a" (rc)
+                : "0" (__NR_futex), 
+                  "b" (uaddr), "c" (op), "d" (val), "S" (timeout), "D" (0)
+                : "memory", "cc");
+#else
+#error need to define mtcp_futex
+#endif
+}
+
+#endif
