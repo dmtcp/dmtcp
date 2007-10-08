287f6032eed2c68d501e24d87cba531ec1deef39
mrieker <mrieker@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2007-10-08 19:23:46 +0000

    mtcp.c:   add MTCP_SHOWTIMING envar to show checkpoint stats (so I have something to     see when a checkpoint is made)   change using scandir to using getdents when scanning /proc/self/fd directory     to avoid munging malloc state during checkpoint
    mtcp_sys.h:
      add mtcp_sys_getdents[64] definitions
    
    testmtcp4.c:
      add variable length mallocing to better test malloc
      add command line param to specify maximum number of iterations
      free random packets (with 0.25 probability) to better test malloc
    
    Makefile:
      add -g option to always include debug symbols
      create mtcp.lis listing file

--- trunk/mtcp/mtcp_sys.h
+++ trunk/mtcp/mtcp_sys.h
@@ -282,6 +282,12 @@ extern int mtcp_sys_errno;
 #define mtcp_sys_mprotect(args...)  mtcp_inline_syscall(mprotect,3,args)
 #define mtcp_sys_set_tid_address(args...)  mtcp_inline_syscall(set_tid_address,1,args)
 #define mtcp_sys_brk(args...)  mtcp_inline_syscall(brk,1,args)
+#ifdef __NR_getdents
+#define mtcp_sys_getdents(args...)  mtcp_inline_syscall(getdents,3,args)
+#endif
+#ifdef __NR_getdents64
+#define mtcp_sys_getdents64(args...)  mtcp_inline_syscall(getdents64,3,args)
+#endif
 /* These functions are not defined for x86_64. */
 #ifdef __i386__
 # define mtcp_sys_get_thread_area(args...) \
