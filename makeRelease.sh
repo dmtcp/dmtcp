a02ec66976f0a31d2cb992423a7d615719f606c4
Jason Ansel <jansel@users.noreply.github.com>
2008-06-13 23:42:35 +0000

    - Added makeRelease.sh script - Cleaned up TODO

---
+++ makeRelease.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+VERSION=0.99
+
+#run a command with error checking
+function e(){
+  echo "$@" >&2
+  $@ || (echo "ERROR '$@' failed!">&2; exit 1)
+}
+
+#get svn revision number
+function getRev(){
+  if [[ -z "$1" ]]
+  then
+    getRev .
+  else
+    (cd $1 && e svn info) | grep '^Revision: [0-9]*' | cut -d ' ' -f 2  
+  fi
+}
+
+#list a dirs named ".svn"
+function removeSvnDirs(){
+  find $@ -type d | grep '[.]svn$' | xargs rm -rf
+}
+
+
+OLDDIR=`pwd`
+
+STAGING=/tmp/dmtcp_release_staging_`whoami`
+mkdir -p $STAGING
+e cd $STAGING
+
+e svn co https://dmtcp.svn.sourceforge.net/svnroot/dmtcp dmtcp_staging
+
+REV=`getRev dmtcp_staging`
+NAME=dmtcp_$VERSION-r$REV
+
+e mv dmtcp_staging $NAME
+e rm -f $NAME/makeRelease.sh
+e removeSvnDirs
+e tar cf $NAME.tar $NAME
+e gzip -9 $NAME.tar
+e rm -rf $NAME
+e mv $NAME.tar.gz $OLDDIR
+e cd $OLDDIR
+e rm -rf $STAGING
+e ls -al $NAME.tar.gz
+
