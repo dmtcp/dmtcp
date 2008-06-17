5622a5dc1cfd90a33b567f7bf45ea68b6a367b5d
Jason Ansel <jansel@users.noreply.github.com>
2008-06-17 17:51:46 +0000

    - Improved debian packaging. - Added manpage

--- dmtcp/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dmtcp package
-#
-0 4	* * *	root	dmtcp_maintenance
--- dmtcp/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dmtcp):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="dmtcp" command="/usr/bin/dmtcp"
