d6b4f033f36bf09b921a9c2327cbcbe37b43ddca
jansel0 <jansel0@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2008-06-25 21:40:59 +0000

    - Add new dmtcpaware tests

---
+++ trunk/test/dmtcpaware1.c
@@ -0,0 +1,57 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "../dmtcp/src/dmtcpaware.h"
+
+void pre(){
+  printf("HOOK: preCheckpoint\n");
+}
+
+void post(){
+  printf("HOOK: postCheckpoint\n");
+}
+
+void restart(){
+  printf("HOOK: postRestart\n");
+}
+
+int main(int argc, char* argv[])
+{
+  if(dmtcpIsEnabled())
+    dmtcpInstallHooks(pre,post,restart);
+
+  int r;
+  const DmtcpCoordinatorStatus* cs;
+  const DmtcpLocalStatus * ls;
+  while (1)
+  {
+    printf("dmtcpIsEnabled()=");
+    fflush(stdout);
+    printf("%d\n", r=dmtcpIsEnabled());
+    assert(r);
+
+    printf("dmcpRunCommand('l')=");
+    fflush(stdout);
+    printf("%d\n",r=dmtcpRunCommand('l'));
+    assert(r);
+
+    printf("dmtcpGetLocalStatus()=");
+    fflush(stdout);
+    ls = dmtcpGetLocalStatus();
+    assert(ls!=NULL);
+    printf("{\n\t %d,\n\t %d,\n\t %s,\n\t %s,\n\t %s}\n", 
+        ls->numCheckpoints, ls->numRestarts, ls->checkpointFilenameMtcp,
+        ls->checkpointFilenameDmtcp, ls->uniquePidStr);
+
+    printf("dmtcpGetCoordinatorStatus()=");
+    fflush(stdout);
+    cs = dmtcpGetCoordinatorStatus();
+    assert(cs!=NULL);
+    printf("{%d,%d}\n", cs->numProcesses, cs->isRunning);
+    assert(cs->numProcesses>0);
+
+    sleep(2);
+
+  }
+  return 0;
+}
