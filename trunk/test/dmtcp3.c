41dd0db0fee7d64c06610a0172ef558aa3397006
jansel0 <jansel0@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2008-07-14 21:23:31 +0000

    - added test dmtcp3, pthread_cond_wait stress test

---
+++ trunk/test/dmtcp3.c
@@ -0,0 +1,53 @@
+#include <errno.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void *threadMain(void *dummy);
+
+#define N 20
+
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int numWaiting = 0;
+
+int main ()
+{
+  int x[N], i;
+  pthread_t t[N];
+
+  for (i = 0; i < N-1; i ++) {
+    x[i]=i;
+    if (pthread_create (&t[i], NULL, &threadMain, x+i) < 0) {
+      fprintf (stderr, "error creating thread: %s\n", strerror (errno));
+      return (-1);
+    }
+  }
+  x[N-1]=N-1;
+  threadMain(x+N-1);
+  return (0);
+}
+
+static void *threadMain (void *_n)
+{
+  int* n = (int*) _n;
+  int count = 0;
+
+  while (1) {
+    pthread_mutex_lock(&mutex);
+    if(numWaiting == N-1)
+      pthread_cond_signal(&cond);
+    
+    numWaiting++;
+    pthread_cond_wait(&cond, &mutex);
+    numWaiting--;
+    pthread_mutex_unlock(&mutex);
+
+    printf("thread%d: %d\n", *n, count++);
+    //sleep(2);
+  }
+
+  return (NULL);
+}
