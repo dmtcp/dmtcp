586f6f775c54694278eec944d6d4d5bd2643390a
gdc0 <gene@ccs.neu.edu>
2009-07-15 00:28:52 +0000

    Changed QUICK-START description of status of matlab.  Checkpointing matlab does work if one uses dash instead of bash. With bash, we continue to get environment variable probelms and sometimes a  bash/malloc/malloc.s error about freeing an already freed block.

--- dmtcp/src/dmtcpworker.cpp
+++ dmtcp/src/dmtcpworker.cpp
@@ -98,10 +98,10 @@ dmtcp::DmtcpWorker::DmtcpWorker ( bool enableCheckpointing )
     ,_restoreSocket ( PROTECTEDFD ( 3 ) )
 {
   if ( !enableCheckpointing ) return;
-  if ( getenv("JALIB_UTILITY_DIR") == NULL ) {
+  if ( getenv(ENV_VAR_UTILITY_DIR) == NULL ) {
     JNOTE ( "\n **** Not checkpointing this process,"
             " due to missing environment var ****" )
-          ( getenv("JALIB_UTILITY_DIR") )
+          ( getenv(ENV_VAR_UTILITY_DIR) )
           ( jalib::Filesystem::GetProgramName() );
     return;
   }
