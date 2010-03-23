2c55de4fb209d1ba5fd67c61179acd4dc3df081e
karya <karya@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2010-03-23 07:17:55 +0000

    --sleep-interval for dmtcp_restart_script_wrapper.sh to specify the time in seconds that the script should wait before testing if the process is alive. The script is still pretty rough.

--- trunk/utils/dmtcp_restart_script_wrapper.sh
+++ trunk/utils/dmtcp_restart_script_wrapper.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+sleep_interval=15
+if [ $1 = "--sleep-interval" ]; then
+  sleep_interval="$2"
+  echo "Sleep interval specified: $sleep_interval seconds"
+  shift
+  shift
+fi
+
 if [ "$#" == 0 ]; then
   file="`pwd`/dmtcp_restart_script.sh"
 else
   file=$1
 fi
+
 restart_file_path=`readlink $file`
 restart_file_name=`basename $restart_file_path .sh`
 comp_group=${restart_file_name#dmtcp_restart_script_}
@@ -25,29 +34,33 @@ echo $tmp_dir
 mkdir -p $tmp_dir
 
 file_path=$tmp_dir/$file_name
-rm -rf $file_path
 
-if [ "$#" == 0 ];then
-  /bin/bash $file &
-else
-  /bin/bash "$@" &
-fi
+while true
+do
+  rm -rf $file_path
 
-cpid=$!
-
-echo "Sleeping for 4 seconds"
-sleep 4
-
-if [ -f $file_path ]; then
-  child_pid=`cat $file_path`
-  if [ $cpid != $child_pid ]; then
-    echo "invalid contents: contents of $file_path: \"`cat $file_path`\"; expected: %cpid"
-    exit
+  if [ "$#" == 0 ];then
+    /bin/bash $file &
+  else
+    /bin/bash "$@" &
   fi
-  wait $cpid
-else
-  echo "process died"
-  kill -9 $cpid
-  wait $cpid
-  exit
-fi
+
+  cpid=$!
+
+  echo "Sleeping for $sleep_interval seconds"
+  sleep $sleep_interval
+
+  if [ -f $file_path ]; then
+    child_pid=`cat $file_path`
+    if [ $cpid != $child_pid ]; then
+      echo "invalid contents: contents of $file_path: \"`cat $file_path`\"; expected: %cpid"
+      exit
+    fi
+    wait $cpid
+    break
+  else
+    echo "process died, restarting it"
+    kill -9 $cpid
+    wait $cpid
+  fi
+done
