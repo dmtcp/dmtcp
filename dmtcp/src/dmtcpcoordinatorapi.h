2b18fa523dc45de8def96d38ea098e9996fdd59a
gdc0 <gene@ccs.neu.edu>
2010-05-30 06:39:23 +0000

    This fixes a bug in which a process could fail in doing the first checkpoint about one time in a thousand (more often if machine is heavily loaded). The bug was caused by an interaction with the exit bug fix in CleanupWorker().     This revision creates a new class DmtcpCoordinatorAPI all of whose methods are duplicates of certain methods of DmtcpWorker.  In a later revision, DmtcpWorker will inherit from DmtcpCoordinatorAPI to remove the duplication.     The issue is that dmtcpapi.cpp and dmtcp_command.cpp create an extra worker with argument enableCheckpointing set to false.  They do so to be able to use the DmtcpWorker method connectAndSendUserCommand() to send a command to the coordinator.  But when this extra worker calls the destructor, the DmtcpWorker destructor calls CleanupWorker(). CleanupWorker then sets the WorkerState to UNKNOWN, which sometimes is transmitted to the coordinator before the worker state can change again.     The files dmtcpapi.cpp and dmtcp_command.cpp now do all of their communication with the coordinator using this new class.  The new class never sets any worker state to UNKNOWN.  It is also a cleaner design, separating out this functionality from DmtcpWorker, which was getting too large. Where locks are used (to delay checkpointing during communication with coordinator), methods from the old DmtcpWorker class are still used for those locks.     There are still a small number of other calls to the DmtcpWorker constructor for temporary communication with the coordinator (for example, to inquire about status with the 's' command).  These calls invoke the constructor with argument false, and so are easy to find.  Those will also be replaced by a call to the DmtcpCoordinatorAPI constructor in a later revision after further testing.

---
+++ dmtcp/src/dmtcpcoordinatorapi.h
@@ -0,0 +1,57 @@
+/****************************************************************************
+ *   Copyright (C) 2006-2010 by Jason Ansel, Kapil Arya, and Gene Cooperman *
+ *   jansel@csail.mit.edu, kapil@ccs.neu.edu, gene@ccs.neu.edu              *
+ *                                                                          *
+ *   This file is part of the dmtcp/src module of DMTCP (DMTCP:dmtcp/src).  *
+ *                                                                          *
+ *  DMTCP:dmtcp/src is free software: you can redistribute it and/or        *
+ *  modify it under the terms of the GNU Lesser General Public License as   *
+ *  published by the Free Software Foundation, either version 3 of the      *
+ *  License, or (at your option) any later version.                         *
+ *                                                                          *
+ *  DMTCP:dmtcp/src is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU Lesser General Public License for more details.                     *
+ *                                                                          *
+ *  You should have received a copy of the GNU Lesser General Public        *
+ *  License along with DMTCP:dmtcp/src.  If not, see                        *
+ *  <http://www.gnu.org/licenses/>.                                         *
+ ****************************************************************************/
+
+#ifndef DMTCPDMTCPCOMMANDAPI_H
+#define DMTCPDMTCPCOMMANDAPI_H
+
+#include  "../jalib/jsocket.h"
+#include "../jalib/jalloc.h"
+#include "dmtcpmessagetypes.h"
+#include "constants.h"
+
+namespace dmtcp
+{
+
+  class ConnectionState;
+
+  class DmtcpCoordinatorAPI
+  {
+    public:
+      DmtcpCoordinatorAPI ();
+      // Accept default destructor
+
+      void connectAndSendUserCommand(char c, int* result = NULL);
+
+      void useAlternateCoordinatorFd();
+
+      bool connectToCoordinator(bool dieOnError=true);
+      bool tryConnectToCoordinator();
+
+    protected:
+      void sendUserCommand(char c, int* result = NULL);
+
+      jalib::JSocket _coordinatorSocket;
+      jalib::JSocket _restoreSocket;
+  };
+
+}
+
+#endif
