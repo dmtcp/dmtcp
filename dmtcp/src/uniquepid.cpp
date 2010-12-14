9b7a3cde02f0313294b8e9e443f2560b3718a374
avisan <amvisan@ccs.neu.edu>
2010-12-14 00:02:49 +0000

    The code failed to compile with the following flags:
    ./configure --enable-ptrace-support --enable-unique-checkpoint-filenames
    
    Thanks, Xin!

--- dmtcp/src/uniquepid.cpp
+++ dmtcp/src/uniquepid.cpp
@@ -19,6 +19,7 @@
  *  <http://www.gnu.org/licenses/>.                                         *
  ****************************************************************************/
 
+#include "util.h"
 #include "uniquepid.h"
 #include <stdlib.h>
 #include <string.h>
