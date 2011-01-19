1a9649b99b354cadbc8fc1c4830fe59eab626847
karya <karya@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2011-01-19 02:15:25 +0000

    - waitpid() wrapper works now. - More information provided as comments in safe_real_waitpid() function.

--- trunk/dmtcp/src/pidwrappers.cpp
+++ trunk/dmtcp/src/pidwrappers.cpp
@@ -373,37 +373,47 @@ static int get_sigckpt() {
 }
 }
 
-extern "C" {
-static pid_t safe_real_waitpid(pid_t pid, int *stat_loc, int options) {
+extern "C"
+pid_t safe_real_waitpid(pid_t pid, int *stat_loc, int options) {
   // Note that if the action for SIGCHLD is set to SIG_IGN, then waitpid fails
   //  with errno set to ECHLD (as if the child process was not really our child)
   //  We currently do not specially handle this case.
-  int verify = 1;
   while (1) {
-    pid_t currPid;
-    pid_t retval;
-    do {
-      currPid = originalToCurrentPid(pid);
-      retval = _real_waitpid(currPid, stat_loc, options);
-    } while( WIFSIGNALED(*stat_loc) && WTERMSIG(*stat_loc) == get_sigckpt() );
+    /* The checkpoint signal uses SA_RESTART which causes the system call to
+     * restart if it were interrupted by this signal. However, on restart, the
+     * currPid will not be correct anymore and hence the restarted waitpid()
+     * will fail with ECHILD.
+     * TODO: Note that the currPid after restart might be equal to the
+     *       current-pid of some other child process resulting in waitpid
+     *       waiting un-necessarily for getting status of that other child
+     *       process. This could be fixed in the sa_restorer function of
+     *       sigaction in mtcp/mtcp_sigaction.c.
+     */
+    pid_t currPid = originalToCurrentPid(pid);
+    pid_t retval = _real_waitpid(currPid, stat_loc, options);
 
-    // Verify there was no checkpoint/restart between currpid and _real_waitpid.
-    // Note:  A checkpoint/restart after _real_waitpid does not need a verify. 
-    if ( verify &&
-         retval == -1 && errno == ECHILD && ! WIFSIGNALED(*stat_loc) &&
+    WRAPPER_EXECUTION_DISABLE_CKPT();
+
+    /* Verify there was no checkpoint/restart between currpid and _real_waitpid.
+     * Note:  A checkpoint/restart after _real_waitpid does not need a verify. 
+     * TODO: There is a possible bug -- if by conincidence the currentPid
+     *       becomes equal to curPid, this will cause the if condition to go
+     *       false, which is wrong.
+     */
+    if ( retval == -1 && errno == ECHILD && ! WIFSIGNALED(*stat_loc) &&
          currPid != originalToCurrentPid(pid) ) {
       struct sigaction oldact;
       if ( sigaction(SIGCHLD, NULL, &oldact) == 0 &&
            oldact.sa_handler != SIG_IGN ) {
-        verify = 0;
-        continue; // Continue to verify, but only this once
+        WRAPPER_EXECUTION_ENABLE_CKPT();
+        continue;
       }
     }
-    // No need to verify.  Just return.
+    WRAPPER_EXECUTION_ENABLE_CKPT();
     return retval;
   }
 }
-}
+
 
 extern "C" pid_t waitpid(pid_t pid, int *stat_loc, int options)
 {
