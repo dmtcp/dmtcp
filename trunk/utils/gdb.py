82b99c07db27a7717382bc46c2b6b3f72aceb5dc
karya <karya@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2011-04-29 19:38:09 +0000

    - util/gdb.py updated to include add_symbol_file command.

--- trunk/utils/gdb.py
+++ trunk/utils/gdb.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
+# Here are some gdb commands written in python using the gdb Python API described here:
+#    http://sourceware.org/gdb/current/onlinedocs/gdb/Python-API.html#Python-API
+
 from __future__ import with_statement
 import subprocess
 import string
+import os
 import gdb
 
 class ProcSelfFd(gdb.Command):
@@ -33,11 +37,11 @@ ProcSelfMaps()
 
 class CallerIs (gdb.Function):
     """Return True if the calling function's name is equal to a string.
-This function takes one or two arguments.
-The first argument is the name of a function; if the calling function's
-name is equal to this argument, this function returns True.
-The optional second argument tells this function how many stack frames
-to traverse to find the calling function.  The default is 1."""
+    This function takes one or two arguments.
+    The first argument is the name of a function; if the calling function's
+    name is equal to this argument, this function returns True.
+    The optional second argument tells this function how many stack frames
+    to traverse to find the calling function.  The default is 1."""
 
     def __init__ (self):
         super (CallerIs, self).__init__ ("caller_is")
@@ -50,3 +54,49 @@ to traverse to find the calling function.  The default is 1."""
         return frame.get_name () == name.string ()
 
 CallerIs()
+
+
+class AddSymbolFile(gdb.Command):
+    """ This command will load the debugging information about a dynamic
+    library (.so file) using gdb's add-symbol-file command"""
+
+    def __init__(self):
+        gdb.Command.__init__(self, "add_symbol_file", gdb.COMMAND_SUPPORT)
+
+    def invoke(self, arg, from_tty):
+        pid = str(gdb.inferiors()[0].pid)
+        cmd = "cat /proc/" + pid + "/maps | grep "
+        res = subprocess.Popen(cmd + arg,
+                               shell=True, stdout=subprocess.PIPE).communicate()[0]
+        if (len(res) == 0):
+            print '**** Library %s not found. ' % arg
+            print '  Do:  cat /proc/%d/maps to see all libs.' % pid
+            return
+
+        lib=subprocess.Popen(cmd + arg + " | head -1 | sed -e 's%[^/]*\\(/.*\\)%\\1%'",
+                             shell=True, stdout=subprocess.PIPE).communicate()[0].rstrip()
+
+        segAddr=subprocess.Popen(cmd + lib + "| grep r-xp |"\
+                                 "sed -e 's%^\\([0-9a-f]*\\).*%0x\\1%'",
+                                 shell=True, stdout=subprocess.PIPE).communicate()[0]
+        segDataAddr=subprocess.Popen(cmd + lib +
+                                " | grep rw-p | sed -e 's%^\\([0-9a-f]*\\).*%0x\\1%'",
+                                 shell=True, stdout=subprocess.PIPE).communicate()[0]
+
+        textOffset=subprocess.Popen("readelf -S " + lib +" | grep '\.text ' | " \
+                                    "sed -e 's%^.*text[^0-9a-f]*[0-9a-f]*\\s*\\([0-9a-f]*\\).*%0x\\1%'",
+                                    shell=True, stdout=subprocess.PIPE).communicate()[0]
+        dataOffset=subprocess.Popen("readelf -S " + lib +" | grep '\.data ' | "\
+                                    "sed -e 's%^.*data[^0-9a-f]*[0-9a-f]*\\s*\\([0-9a-f]*\\).*%0x\\1%'",
+                                    shell=True, stdout=subprocess.PIPE).communicate()[0]
+        bssOffset=subprocess.Popen("readelf -S " + lib +" | grep '\.bss ' | "\
+                                   "sed -e 's%^.*bss[^0-9a-f]*[0-9a-f]*\\s*\\([0-9a-f]*\\).*%0x\\1%'",
+                                   shell=True, stdout=subprocess.PIPE).communicate()[0]
+
+        gdb.execute("add-symbol-file " + lib + " " + str(long(segAddr, 16) +
+                                                         long(textOffset, 16))
+                    + " -s .data " + str(long(segDataAddr, 16) + long(dataOffset, 16))
+                    + " -s .bss " + str(long(segDataAddr, 16) + long(bssOffset, 16)),
+                    True, True) 
+
+AddSymbolFile()
