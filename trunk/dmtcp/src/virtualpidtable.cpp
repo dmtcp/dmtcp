20944f5eef971726babb5f844404b71173e6767e
gdc0 <gdc0@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2011-08-10 16:33:11 +0000

    - Backing out of my revision 1253.  It was a mistake.

--- trunk/dmtcp/src/virtualpidtable.cpp
+++ trunk/dmtcp/src/virtualpidtable.cpp
@@ -522,7 +522,7 @@ void dmtcp::VirtualPidTable::InsertIntoPidMapFile( pid_t originalPid, pid_t curr
 
   pidMapFile =  jalib::Filesystem::ResolveSymlink ( pidMapFile );
   pidMapCountFile =  jalib::Filesystem::ResolveSymlink ( pidMapCountFile );
-  JASSERT ( pidMapFile.length() > 0 || pidMapCountFile.length() > 0 )
+  JASSERT ( pidMapFile.length() > 0 && pidMapCountFile.length() > 0 )
     ( pidMapFile )( pidMapCountFile ).Text("Failed to resolve symlink.");
 
   // Create Serializers
