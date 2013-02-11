a40e3349f55802d79e09646986c06e678864bd1e
Kapil Arya <kapil@ccs.neu.edu>
2013-02-11 08:13:14 +0000

    Minor fix for SysV IPC plugin.

--- plugin/ipc/sysv/sysvipcwrappers.cpp
+++ plugin/ipc/sysv/sysvipcwrappers.cpp
@@ -37,7 +37,7 @@ int shmget(key_t key, size_t size, int shmflg)
 {
   int realId = -1;
   int virtId = -1;
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   realId = _real_shmget(key, size, shmflg);
   if (realId != -1) {
     dmtcp::SysVIPC::instance().on_shmget(realId, key, size, shmflg);
@@ -45,14 +45,14 @@ int shmget(key_t key, size_t size, int shmflg)
     JTRACE ("Creating new Shared memory segment")
       (key) (size) (shmflg) (realId) (virtId);
   }
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return virtId;
 }
 
 extern "C"
 void *shmat(int shmid, const void *shmaddr, int shmflg)
 {
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   int realShmid = VIRTUAL_TO_REAL_IPC_ID(shmid);
   JASSERT(realShmid != -1) .Text("Not Implemented");
   void *ret = _real_shmat(realShmid, shmaddr, shmflg);
@@ -60,31 +60,31 @@ void *shmat(int shmid, const void *shmaddr, int shmflg)
     dmtcp::SysVIPC::instance().on_shmat(shmid, shmaddr, shmflg, ret);
     JTRACE ("Mapping Shared memory segment") (shmid) (realShmid) (shmflg) (ret);
   }
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return ret;
 }
 
 extern "C"
 int shmdt(const void *shmaddr)
 {
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   int ret = _real_shmdt(shmaddr);
   if (ret != -1) {
     dmtcp::SysVIPC::instance().on_shmdt(shmaddr);
     JTRACE ("Unmapping Shared memory segment" ) (shmaddr);
   }
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return ret;
 }
 
 extern "C"
 int shmctl(int shmid, int cmd, struct shmid_ds *buf)
 {
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   int realShmid = VIRTUAL_TO_REAL_IPC_ID(shmid);
   JASSERT(realShmid != -1);
   int ret = _real_shmctl(realShmid, cmd, buf);
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return ret;
 }
 
@@ -99,14 +99,14 @@ int semget(key_t key, int nsems, int semflg)
 {
   int realId = -1;
   int virtId = -1;
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   realId = _real_semget (key, nsems, semflg);
   if (realId != -1) {
     dmtcp::SysVIPC::instance().on_semget(realId, key, nsems, semflg);
     virtId = REAL_TO_VIRTUAL_IPC_ID(realId);
     JTRACE ("Creating new SysV Semaphore" ) (key) (nsems) (semflg);
   }
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return virtId;
 }
 
@@ -134,14 +134,14 @@ int semtimedop(int semid, struct sembuf *sops, size_t nsops,
 
   if (ipc_nowait_specified ||
       (timeout != NULL && TIMESPEC_CMP(timeout, &ts_100ms, <))) {
-    WRAPPER_EXECUTION_DISABLE_CKPT();
+    DMTCP_DISABLE_CKPT();
     realId = VIRTUAL_TO_REAL_IPC_ID(semid);
     JASSERT(realId != -1);
     ret = _real_semtimedop(realId, sops, nsops, timeout);
     if (ret == 0) {
       dmtcp::SysVIPC::instance().on_semop(semid, sops, nsops);
     }
-    WRAPPER_EXECUTION_ENABLE_CKPT();
+    DMTCP_ENABLE_CKPT();
     return ret;
   }
 
@@ -151,14 +151,14 @@ int semtimedop(int semid, struct sembuf *sops, size_t nsops,
    */
   while (timeout == NULL || TIMESPEC_CMP(&totaltime, timeout, <)) {
     ret = EAGAIN;
-    WRAPPER_EXECUTION_DISABLE_CKPT();
+    DMTCP_DISABLE_CKPT();
     realId = VIRTUAL_TO_REAL_IPC_ID(semid);
     JASSERT(realId != -1);
     ret = _real_semtimedop(realId, sops, nsops, &ts_100ms);
     if (ret == 0) {
       dmtcp::SysVIPC::instance().on_semop(semid, sops, nsops);
     }
-    WRAPPER_EXECUTION_ENABLE_CKPT();
+    DMTCP_ENABLE_CKPT();
 
     // TODO Handle EIDRM
     if (ret == 0 ||
@@ -181,14 +181,14 @@ int semctl(int semid, int semnum, int cmd, ...)
   uarg = va_arg (arg, union semun);
   va_end (arg);
 
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   int realId = VIRTUAL_TO_REAL_IPC_ID(semid);
   JASSERT(realId != -1);
   int ret = _real_semctl(realId, semnum, cmd, uarg);
   if (ret != -1) {
     dmtcp::SysVIPC::instance().on_semctl(semid, semnum, cmd, uarg);
   }
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return ret;
 }
 /******************************************************************************
@@ -202,14 +202,14 @@ int msgget(key_t key, int msgflg)
 {
   int realId = -1;
   int virtId = -1;
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   realId = _real_msgget (key, msgflg);
   if (realId != -1) {
     dmtcp::SysVIPC::instance().on_msgget(realId, key, msgflg);
     virtId = REAL_TO_VIRTUAL_IPC_ID(realId);
     JTRACE ("Creating new SysV Msg Queue" ) (key) (msgflg);
   }
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return virtId;
 }
 
@@ -225,14 +225,14 @@ int msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg)
    * If IPC_NOWAIT was specified and msgsnd fails with EAGAIN, return.
    */
   while (true) {
-    WRAPPER_EXECUTION_DISABLE_CKPT();
+    DMTCP_DISABLE_CKPT();
     realId = VIRTUAL_TO_REAL_IPC_ID(msqid);
     JASSERT(realId != -1);
     ret = _real_msgsnd(realId, msgp, msgsz, msgflg | IPC_NOWAIT);
     if (ret == 0) {
       dmtcp::SysVIPC::instance().on_msgsnd(msqid, msgp, msgsz, msgflg);
     }
-    WRAPPER_EXECUTION_ENABLE_CKPT();
+    DMTCP_ENABLE_CKPT();
 
     // TODO Handle EIDRM
     if ((ret == 0) ||
@@ -259,14 +259,14 @@ ssize_t msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg)
    * If IPC_NOWAIT was specified and msgsnd fails with EAGAIN, return.
    */
   while (true) {
-    WRAPPER_EXECUTION_DISABLE_CKPT();
+    DMTCP_DISABLE_CKPT();
     realId = VIRTUAL_TO_REAL_IPC_ID(msqid);
     JASSERT(realId != -1);
     ret = _real_msgrcv(realId, msgp, msgsz, msgtyp, msgflg | IPC_NOWAIT);
     if (ret == 0) {
       dmtcp::SysVIPC::instance().on_msgrcv(msqid, msgp, msgsz, msgtyp, msgflg);
     }
-    WRAPPER_EXECUTION_ENABLE_CKPT();
+    DMTCP_ENABLE_CKPT();
 
     // TODO Handle EIDRM
     if ((ret >= 0) ||
@@ -285,13 +285,13 @@ ssize_t msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg)
 extern "C"
 int msgctl(int msqid, int cmd, struct msqid_ds *buf)
 {
-  WRAPPER_EXECUTION_DISABLE_CKPT();
+  DMTCP_DISABLE_CKPT();
   int realId = VIRTUAL_TO_REAL_IPC_ID(msqid);
   JASSERT(realId != -1);
   int ret = _real_msgctl(realId, cmd, buf);
   if (ret != -1) {
     dmtcp::SysVIPC::instance().on_msgctl(msqid, cmd, buf);
   }
-  WRAPPER_EXECUTION_ENABLE_CKPT();
+  DMTCP_ENABLE_CKPT();
   return ret;
 }
