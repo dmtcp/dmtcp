d8dd402f6df1ec2ebd912aa664d368c49b135a60
Kapil Arya <kapil@ccs.neu.edu>
2013-07-24 03:14:17 +0000

    IPC Plugin: Make sendFd/receiveFd more generic.
    It would be used by the ssh plugin as well.

---
+++ plugin/ipc/util_ipc.h
@@ -0,0 +1,15 @@
+#ifndef UTIL_SSH_H
+#define UTIL_SSH_H
+
+#include <sys/socket.h>
+#include <sys/un.h>
+#include "ipc.h"
+#include "dmtcpplugin.h"
+
+LIB_PRIVATE
+int sendFd(int restoreFd, int fd, void *data, size_t len,
+            struct sockaddr_un& addr, socklen_t addrLen) __attribute((weak));
+LIB_PRIVATE
+int receiveFd(int restoreFd, void *data, size_t len) __attribute((weak));
+
+#endif
