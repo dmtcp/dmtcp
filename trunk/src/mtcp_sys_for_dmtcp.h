01de5c3a2c676ab807d449badf8a22233a32f105
gdc0 <gdc0@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2014-02-11 18:27:09 +0000

    Fix compile bug for ARM (introduced at rev. 2523)

--- trunk/src/mtcp_sys_for_dmtcp.h
+++ trunk/src/mtcp_sys_for_dmtcp.h
@@ -12,3 +12,14 @@
 # include <linux/unistd.h>
 # include <asm/ldt.h>
 #endif
+
+#ifdef __arm__
+# if 1
+/* Next macro uses 'mcr', a kernel-mode instr. on ARM */
+#  define mtcp_sys_kernel_set_tls(args...)  \
+    syscall(__ARM_NR_set_tls,args)
+# else
+   /* else inline kernel call */
+#  include "mtcp/mtcp_sys.h"
+# endif
+#endif
