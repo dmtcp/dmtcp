e401b2f6603e2b803911db1b3ef58ebe017cd2cc
gdc0 <gdc0@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2014-02-15 06:13:59 +0000

    Imrovements for readdmtcp.sh

--- trunk/util/readdmtcp.sh
+++ trunk/util/readdmtcp.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-mtcp_restart --simulate $1
+if file $1 | grep gzip > /dev/null; then
+  echo '***' $1 is a gzipped file.  Please uncompress it first.
+  exit 0
+fi
+
+if which mtcp_restart > /dev/null; then
+  mtcp_restart --simulate $1
+  exit 0
+fi
+
+if test -x bin/mtcp_restart; then
+  bin/mtcp_restart --simulate $1
+  exit 0
+fi
+
+if test -x ../../bin/mtcp_restart; then
+  ../../bin/mtcp_restart --simulate $1
+  exit 0
+fi
