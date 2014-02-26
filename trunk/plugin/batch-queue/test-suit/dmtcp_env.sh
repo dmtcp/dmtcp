9f5e338395f7f9f95006342c2cb0d6ef0d88bffc
artpol <artpol@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2014-02-26 05:53:28 +0000

    [RM plugin] Test suit

--- trunk/plugin/batch-queue/test-suit/dmtcp_env.sh
+++ trunk/plugin/batch-queue/test-suit/dmtcp_env.sh
@@ -3,7 +3,3 @@
 # DMTCP settings
 export PATH="/home/research/artpol/sandbox/bin/:$PATH"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/research/artpol/sandbox/lib"
-
-# OpenMPI
-export PATH="/home/research/artpol/openmpi_build/bin/:$PATH"
-export LD_LIBRARY_PATH="/home/research/artpol/openmpi_build/lib:$LD_LIBRARY_PATH"
---
+++ trunk/plugin/batch-queue/test-suit/mpich2_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# MPICH2 settings
+export PATH="/home/research/artpol/openmpi_build/bin/:$PATH"
+export LD_LIBRARY_PATH="/home/research/artpol/openmpi_build/lib:$LD_LIBRARY_PATH"
