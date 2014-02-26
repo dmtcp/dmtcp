ca9e4aa110991f47fedead11773b15b929b1c2f6
artpol <artpol84@gmail.com>
2014-02-26 05:19:04 +0000

    [RM plugin]: Add first version of MPI test suit. The idea is to have all things prepared for testing RM plugin on new cluster right from dmtcp-trunk directory. And to make configuration and preparation steps as easy and fast as possible.

---
+++ plugin/batch-queue/test-suit/patch_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# DMTCP settings
+export PATH="/home/research/artpol/sandbox/bin/:$PATH"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/research/artpol/sandbox/lib"
+
+# OpenMPI
+export PATH="/home/research/artpol/openmpi_build/bin/:$PATH"
+export LD_LIBRARY_PATH="/home/research/artpol/openmpi_build/lib:$LD_LIBRARY_PATH"
