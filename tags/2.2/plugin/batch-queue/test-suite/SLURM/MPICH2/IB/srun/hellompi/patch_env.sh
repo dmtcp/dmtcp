1ad748519505a28ac9dd89aec557a2996537d685
karya <karya@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2014-03-13 16:44:29 +0000

    Untagging 2.2.

--- tags/2.2/plugin/batch-queue/test-suite/SLURM/MPICH2/IB/srun/hellompi/patch_env.sh
+++
@@ -1,3 +0,0 @@
-source ../../../../../dmtcp_env.sh
-source ../../../../../mpich2_env.sh
-source ../../../../../slurm_env.sh
--- tags/2.2/plugin/batch-queue/test-suite/SLURM/OpenMPI/IB/mpirun/hellompi/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -Rf ckpt_* dmtcp_restart_* dmtcp_command.* hellompi_[cr].err hellompi_[cr].std
-rm -Rf LOGS/*
