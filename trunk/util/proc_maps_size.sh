4af8f75423d83b6c5ac80159fcb5e2dceb007e6d
artpol <artpol@ba6dc0ae-232f-0410-883f-9ebac64fc565>
2014-05-01 05:40:55 +0000

    Fix maps size inspector

--- trunk/util/proc_maps_size.sh
+++ trunk/util/proc_maps_size.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]; then
     echo " Not enough params. Wand process PID"
 fi
 
-cat /proc/$1/maps | awk '{ print $1 }' | awk '{ split($0,array,"-"); print "0x"array[2]" 0x"array[1] }' \
-        | awk --non-decimal-data '{ print ($1 - $2) }'
+cat /proc/$1/maps | awk '{ if( $6 == "" ) print "anonym "$1; else print $6" "$1 }' | awk '{ split($2,array,"-"); print $1" 0x"array[2]" 0x"array[1] }' \
+        | awk --non-decimal-data '{ print $1": "($2 - $3) }'
