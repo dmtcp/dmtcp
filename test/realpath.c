9c06a39c2c1e63e80647851cf21f5aa09857ec36
Gene Cooperman <gc00@github.org>
2014-12-22 16:41:08 -0500

    Add realpath test; realpath is versioned symbol

--- test/realpath.c
+++ test/realpath.c
@@ -1,6 +1,7 @@
 #define _XOPEN_SOURCE 500
 #include <limits.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 // POSIX.1-2008 says that NULL will cause memory to be allocated.
 // In the earlier POSIX.1-2001, GNU implementations would return NULL.
@@ -9,12 +10,17 @@
 // A value of _XOPEN_SOURCE of 500 should support this.
 // Configure autotest first to test if this program can succeed without DMTCP.
 int main() {
-  while (1) {
+  int i;
+  for (i = 0; i < (int)2e9; i++) {
     char *path = realpath("/etc/passwd", NULL);
     if (path == NULL)
       abort();
     else
       free(path);
+    if ( i%(int)1e6 == 0 ) {
+      printf(".");
+      fflush(stdout);
+    }
   }
   return 0;
 }
