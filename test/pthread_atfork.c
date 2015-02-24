d593c1a45116f47a0c361b80d4b4b8bd3c334de2
Gene Cooperman <gene@ccs.neu.edu>
2015-02-24 14:12:41 -0500

    Minor fixes for previous commit

--- test/pthread_atfork.c
+++ test/pthread_atfork.c
@@ -1,12 +1,12 @@
 #define _POSIX_SOURCE
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <pthread.h>
 
 #ifdef LIB
+// Generates libpthread_atfork.so
 void prepare(void) { printf("Before fork\n"); }
 void parent(void) { printf("After fork in parent\n"); }
 void child(void) { printf("After fork in child\n"); }
@@ -18,33 +18,25 @@ void myconstructor(void) {
 
 void foo(void) {}
 #else
+// Generates executable:  pthread_atfork
 void foo(void);
-
-static void die ( const char* msg )
-{
-  printf ( "ERROR: %s \n", msg );
-  _exit ( -1 );
-}
 
 int main ( int argc, char** argv )
 {
-  FILE* fp = NULL;
-
-  // Force loading of library compiled with -DLIB
+  // Force linking of library compiled from this file with 'gcc -DLIB ...'
   foo();
 
   int childpid = fork();
   if ( childpid == 0 ) {  // if child
     int count = 0;
-
-    while (1)
-    { printf(" %2d ",count++);
+    while (1) {
+      printf(" %2d ",count++);
       fflush(stdout);
       sleep(2);
     }
   }
-  else  // else parent
-  { waitpid(childpid, NULL, 0);
+  else {  // else parent
+    waitpid(childpid, NULL, 0);
     printf("*** ERROR: child finished early.");
   }
 }
