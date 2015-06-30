a191081b16868587acffb2898b2e2446965f078b
Rohan Garg <rohgarg@ccs.neu.edu>
2015-06-30 00:06:49 -0400

    alarm-plugin: clean the test program

--- contrib/alarm/tests/alarm.c
+++ contrib/alarm/tests/alarm.c
@@ -1,8 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+
+void
+handle_alarm(int sig)
+{
+  exit(EXIT_SUCCESS);
+}
 
 int
 main()
 {
+  signal(SIGALRM, handle_alarm);
   alarm(15);
   while (1);
   return 0;
