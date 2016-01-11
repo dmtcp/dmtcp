a98634c057ce5afe0b74d8bc39f2b919849cba63
jiajuncao <jonnyjjc@gmail.com>
2016-01-11 15:59:48 -0500

    IB: minor fix.

--- contrib/infiniband/ibvidentifier.c
+++ contrib/infiniband/ibvidentifier.c
@@ -1,13 +1,13 @@
 #include "ibvidentifier.h"
 
-ib_qp_id_t * create_ibv_id(int qpn, int lid, void * buffer, int size)
+ibv_qp_id_t * create_ibv_id(int qpn, int lid, void * buffer, int size)
 {
   srand48(getpid() * time(NULL));
   if (size != sizeof(ib_qp_id_t)) {
     return NULL;
   }
 
-  ib_qp_id_t * id = buffer;
+  ibv_qp_id_t * id = buffer;
 
   id->qpn = qpn;
   id->lid = lid;
