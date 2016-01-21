cf9ee1ca57fad25f86e9ff7b533783ea381dba97
jiajuncao <jonnyjjc@gmail.com>
2016-01-21 16:27:01 -0500

    Restore mutex owners on restart.

--- src/plugin/pid/pid.cpp
+++ src/plugin/pid/pid.cpp
@@ -34,6 +34,7 @@ using namespace dmtcp;
 extern "C" pid_t dmtcp_update_ppid();
 
 static string pidMapFile;
+map<pthread_mutex_t*, pid_t> mapMutexVirtTid;
 
 extern "C"
 pid_t dmtcp_real_to_virtual_pid(pid_t realPid)
@@ -181,6 +182,16 @@ static void pidVirt_ThreadExit(DmtcpEventData_t *data)
   VirtualPidTable::instance().erase(tid);
 }
 
+static void pidVirt_RefillTid() {
+  map<pthread_mutex_t*, pid_t>::iterator it;
+
+  for (it = mapMutexVirtTid.begin(); it != mapMutexVirtTid.end(); it++) {
+    if (it->first->__data.__owner != 0) {
+      it->first->__data.__owner = VIRTUAL_TO_REAL_PID(it->second);
+    }
+  }
+}
+
 extern "C" void dmtcp_event_hook(DmtcpEvent_t event, DmtcpEventData_t *data)
 {
   switch (event) {
@@ -207,6 +218,7 @@ extern "C" void dmtcp_event_hook(DmtcpEvent_t event, DmtcpEventData_t *data)
     case DMTCP_EVENT_REFILL:
       if (data->refillInfo.isRestart) {
         pidVirt_PostRestartRefill(data);
+        pidVirt_RefillTid();
       }
       break;
 
