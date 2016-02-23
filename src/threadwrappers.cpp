3b2614d50a3fd9466386aa9ccfd6baeccf2528e3
Rohan Garg <rohgarg@ccs.neu.edu>
2016-02-23 15:29:49 -0500

    threadwrappers: Fix segfault when clone call fails

--- src/threadwrappers.cpp
+++ src/threadwrappers.cpp
@@ -111,7 +111,7 @@ extern "C" int __clone(int (*fn) (void *arg), void *child_stack, int flags,
   if (tid == -1) {
     JTRACE("Clone call failed")(JASSERT_ERRNO);
     ThreadSync::decrementUninitializedThreadCount();
-    delete thread;
+    JALLOC_HELPER_FREE(thread);
   }
 
   WRAPPER_EXECUTION_ENABLE_CKPT();
