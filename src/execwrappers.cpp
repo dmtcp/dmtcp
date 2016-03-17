8beec0f3ccb640fc7ac1826c1c62dafcdc4bc6b8
Kapil Arya <kapil@ccs.neu.edu>
2016-03-16 20:37:41 -0400

    Added wrapper for daemon().

--- src/execwrappers.cpp
+++ src/execwrappers.cpp
@@ -213,6 +213,45 @@ extern "C" pid_t fork()
   return childPid;
 }
 
+extern "C"
+int daemon(int nochdir, int noclose)
+{
+  int fd;
+
+  switch (fork()) {
+    case -1:
+      return (-1);
+    case 0:
+      break;
+    default:
+      _exit(0);
+  }
+
+  if (setsid() == -1) {
+    return (-1);
+  }
+
+  if (!nochdir) {
+    (void)chdir("/");
+  }
+
+  if (!noclose) {
+    fd = open("/dev/null", O_RDWR, 0);
+    if (fd != -1) {
+      dup2(fd, STDIN_FILENO);
+      dup2(fd, STDOUT_FILENO);
+      dup2(fd, STDERR_FILENO);
+      if (fd > 2) {
+        close(fd);
+      }
+    } else {
+      errno = ENODEV;
+      return -1;
+    }
+  }
+  return (0);
+}
+
 extern "C" pid_t vfork()
 {
   JTRACE("vfork wrapper calling fork");
