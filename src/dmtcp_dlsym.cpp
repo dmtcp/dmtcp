b2b5c0033fcccabb54d934e849527d6bb3d07f43
Rohan Garg <rohgarg@ccs.neu.edu>
2016-12-08 17:58:28 -0500

    dmtcp_dlsym: Add support for handling indirect reference functions

--- src/dmtcp_dlsym.cpp
+++ src/dmtcp_dlsym.cpp
@@ -355,10 +355,19 @@ for ( ; *tmp != '\0'; tmp++ ) {
   *tags_p = tags;
   *default_symbol_index_p = default_symbol_index;
 
-  if (default_symbol_index)
-    return tags.base_addr + tags.symtab[default_symbol_index].st_value;
-  else
+  if (default_symbol_index) {
+    if (ELF64_ST_TYPE(tags.symtab[default_symbol_index].st_info) ==
+        STT_GNU_IFUNC) {
+      typedef void* (*fnc)();
+      fnc f =  (fnc)(tags.base_addr +
+                     tags.symtab[default_symbol_index].st_value);
+      return f();
+    } else {
+      return tags.base_addr + tags.symtab[default_symbol_index].st_value;
+    }
+  } else {
     return NULL;
+  }
 }
 
 // Given a pseudo-handle, symbol name, and addr, returns the address of the symbol
