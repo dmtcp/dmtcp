573308020ea0f60249db0ea49306310746a5a41a
Kapil Arya <kapil@mesosphere.io>
2017-01-14 22:30:55 -0500

    Fixed 32-bit binary launch with 64-bit dmtcp_launch.
    The ENV_VAR_HIJACK_LIBRARIES env variable should _always_ contain 64-bit
    libraries for a 64-bit dmtcp_launch whereas ENV_VAR_HIJACK_LIBRARIES_M32
    should contain 32-bit libraries for a 64-bit dmtcp_launch.
    
    In case of a 32-bit dmtcp_launch, the _M32 environment variable is unset
    and ENV_VAR_HIJACK_LIBRARIES points to 32-bit libraries because 32-bit
    dmtcp_launch is _not_ supposed to launch 64-bit binaries.

--- src/dmtcp_launch.cpp
+++ src/dmtcp_launch.cpp
@@ -813,7 +813,7 @@ setLDPreloadLibs(bool is32bitElf)
     for (size_t i = 0; i < numLibs; i++) {
       struct PluginInfo *p = &pluginInfo[i];
       if (*p->enabled) {
-        preloadLibs += Util::getPath(p->lib, is32bitElf) + ":";
+        preloadLibs += Util::getPath(p->lib) + ":";
 #if defined(__x86_64__) || defined(__aarch64__)
         preloadLibs32 += Util::getPath(p->lib, true) + ":";
 #endif // if defined(__x86_64__) || defined(__aarch64__)
@@ -847,7 +847,7 @@ setLDPreloadLibs(bool is32bitElf)
           "  ./configure --enable-m32 && make clean && make -j && "
           "make install\n"
           "  ./configure && make clean && make -j && make install\n");
-    setenv("LD_PRELOAD", preloadLibs.c_str(), 1);
+    setenv("LD_PRELOAD", preloadLibs32.c_str(), 1);
   }
 #endif // if defined(__x86_64__) || defined(__aarch64__)
   JTRACE("getting value of LD_PRELOAD")
