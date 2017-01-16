00e4c163efdc4fd483a656195f11936d5c10b598
Kapil Arya <kapil@ccs.neu.edu>
2017-01-15 20:09:04 -0800

    Removed stale pthread_cond_* wrappers. (#534)
    These wrappers were left over from FReD changes from few years ago.

--- src/miscwrappers.cpp
+++ src/miscwrappers.cpp
@@ -653,6 +653,7 @@ syscall(long sys_num, ...)
   return ret;
 }
 
+#ifdef ENABLE_PTHREAD_COND_WRAPPERS
 // TODO: Move these to a separate pthreadwrappers.cpp file.
 extern "C" int
 pthread_cond_broadcast(pthread_cond_t *cond)
@@ -703,6 +704,7 @@ pthread_mutex_unlock(pthread_mutex_t *mutex)
 {
   return _real_pthread_mutex_unlock(mutex);
 }
+#endif // #ifdef ENABLE_PTHREAD_COND_WRAPPERS
 
 /*
 extern "C" int
