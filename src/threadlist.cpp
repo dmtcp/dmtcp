6c99a2e3fdf82463c78de24a8605f9339306ed08
Kapil Arya <kapil@ccs.neu.edu>
2017-01-15 19:50:03 -0800

    Fixed a bug in signal handler restoration during restart. (#530)
    The signal handlers must be restored before the restart hooks for
    plugins are called because some plugins might perform tasks that could
    potentially generate a signal. For example, the timer plugin may restore
    a timer which will fire right away, and not having an appropriate signal
    handler could kill the process.

--- src/threadlist.cpp
+++ src/threadlist.cpp
@@ -648,11 +648,17 @@ void ThreadList::waitForAllRestored(Thread *thread)
       sem_wait(&semNotifyCkptThread);
     }
 
+    // Now that all threads have been created, restore the signal handler. We
+    // need to do it before calling callbackPostCheckpoint() because that
+    // routine will invoke restart hooks for all plugins. Some of the plugins
+    // might perform tasks that could potentially generate a signal. For
+    // example, the timer plugin may restore a timer which will fire right away,
+    // and not having an appropriate signal handler could kill the process.
+    SigInfo::restoreSigHandlers();
+
     JLOG(DMTCP)("before callbackPostCheckpoint(isRestart=true)");
     callbackPostCheckpoint(true, NULL); //(isRestart,mtcpRestoreArgvStartAddr);
     JLOG(DMTCP)("after callbackPostCheckpoint(isRestart=true)");
-
-    SigInfo::restoreSigHandlers();
 
     /* raise the signals which were pending for the entire process at the time
      * of checkpoint. It is assumed that if a signal is pending for all threads
