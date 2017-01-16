dmtcp/dmtcp

commit 72df6746095958c7d2dbcc8976d425bf5fd1b1c1

Fixed compiler warnings as observed with -Wall -Werror with gcc 6.

src/mtcp/mtcp_restart.c
@@ -563,7 +563,7 @@ static void restorememoryareas(RestoreInfo *rinfo_ptr)
   if (rinfo_ptr->saved_brk != NULL) {
     // Now, we can do the pending mtcp_sys_brk(rinfo.saved_brk).
     // It's now safe to do this, even though it can munmap memory holding rinfo.
-    mtcp_sys_brk(rinfo_ptr->saved_brk);
+    MTCP_ASSERT(mtcp_sys_brk(rinfo_ptr->saved_brk) == 0);
   }
 
 #if defined(__i386__) || defined(__x86_64__)

include/dmtcp.h
@@ -216,13 +216,13 @@ EXTERNC const char* dmtcp_get_tmpdir(void);
 
 EXTERNC const char* dmtcp_get_ckpt_dir(void) __attribute ((weak));
 #define dmtcp_get_ckpt_dir() \
- (dmtcp_get_ckpt_dir ? dmtcp_get_ckpt_dir() : NULL)
+ (dmtcp_get_ckpt_dir ? dmtcp_get_ckpt_dir() : "")
 EXTERNC int dmtcp_set_ckpt_dir(const char *) __attribute ((weak));
 #define dmtcp_set_ckpt_dir(d) \
  (dmtcp_set_ckpt_dir ? dmtcp_set_ckpt_dir(d) : DMTCP_NOT_PRESENT)
 EXTERNC const char* dmtcp_get_coord_ckpt_dir(void) __attribute__ ((weak));
 #define dmtcp_get_coord_ckpt_dir() \
- (dmtcp_get_coord_ckpt_dir ? dmtcp_get_coord_ckpt_dir() : NULL)
+ (dmtcp_get_coord_ckpt_dir ? dmtcp_get_coord_ckpt_dir() : "")
 EXTERNC int dmtcp_set_coord_ckpt_dir(const char* dir) __attribute__ ((weak));
 #define dmtcp_set_coord_ckpt_dir(d) \
  (dmtcp_set_coord_ckpt_dir ? dmtcp_set_coord_ckpt_dir(d) : DMTCP_NOT_PRESENT)

src/barrierinfo.h
@@ -69,8 +69,8 @@ namespace dmtcp
       }
 
       const DmtcpBarrierType type;
-      const string id;
       void (*callback)();
+      const string id;
       const string pluginName;
   };
 

src/constants.h
@@ -167,7 +167,7 @@
   "under certain conditions; see COPYING file for details.\n"
 
 #define HELP_AND_CONTACT_INFO             \
-  "Report bugs to: "PACKAGE_BUGREPORT"\n" \
-  "DMTCP home page: <"PACKAGE_URL">\n"
+  "Report bugs to: " PACKAGE_BUGREPORT "\n" \
+  "DMTCP home page: <" PACKAGE_URL ">\n"
 
 #endif

src/coordinatorapi.cpp
@@ -322,7 +322,7 @@ void CoordinatorAPI::sendMsgToCoordinator(DmtcpMessage msg,
   }
   JASSERT(Util::writeAll(_coordinatorSocket, &msg, sizeof(msg)) == sizeof(msg));
   if (extraData != NULL) {
-    JASSERT(Util::writeAll(_coordinatorSocket, extraData, len) == len);
+    JASSERT(Util::writeAll(_coordinatorSocket, extraData, len) == (ssize_t)len);
   }
 }
 

src/dmtcp_restart.cpp
@@ -797,7 +797,7 @@ int main(int argc, char** argv)
   WorkerState::setCurrentState(WorkerState::RESTARTING);
 
   /* Try to find non-orphaned process in independent procs list */
-  RestoreTarget *t;
+  RestoreTarget *t = NULL;
   bool foundNonOrphan = false;
   RestoreTargetMap::iterator it;
   for (it = independentProcessTreeRoots.begin();
@@ -810,6 +810,7 @@ int main(int argc, char** argv)
     }
   }
 
+  JASSERT(t != NULL);
   JASSERT(t->pid() != 0);
   JASSERT(!t->noCoordinator() || allowedModes == COORD_ANY)
     .Text("Process had no coordinator prior to checkpoint;\n"

src/dmtcpplugin.cpp
@@ -62,7 +62,7 @@ EXTERNC int dmtcp_is_enabled() { return 1; }
 
 EXTERNC int dmtcp_checkpoint()
 {
-  int oldNumRestarts, oldNumCheckpoints;
+  size_t oldNumRestarts, oldNumCheckpoints;
 
   while (1) {
     WRAPPER_EXECUTION_GET_EXCL_LOCK();

src/plugin/ipc/file/fileconnection.cpp
@@ -302,7 +302,7 @@ void FileConnection::refill(bool isRestart)
                   "   Change this function and re-compile, if you want "
                   "different behavior.");
         } else {
-          char *errMsg =
+          const char *errMsg =
             "\n**** File already exists! Checkpointed copy can't be restored.\n"
             "       The Contents of checkpointed copy differ from the "
             "contents of the existing copy.\n"

commit 3f56b3422031d9d56dc830c165d51f13696d4e26

mtcp_restart: Remove assert introduced in 72df6746
The brk call fails on Debian for bash because of alignment conflict
between heap and mtcp_restart.

src/mtcp/mtcp_restart.c
@@ -638,7 +638,9 @@ restorememoryareas(RestoreInfo *rinfo_ptr)
   if (rinfo_ptr->saved_brk != NULL) {
     // Now, we can do the pending mtcp_sys_brk(rinfo.saved_brk).
     // It's now safe to do this, even though it can munmap memory holding rinfo.
-    MTCP_ASSERT(mtcp_sys_brk(rinfo_ptr->saved_brk) == 0);
+    if (mtcp_sys_brk(rinfo_ptr->saved_brk) != 0) {
+       MTCP_PRINTF("error restoring brk: %d\n", mtcp_sys_errno);
+    }
   }
 
 #if defined(__i386__) || defined(__x86_64__)