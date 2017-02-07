bc6f39ece17af1c3a4b955ba0511b0cd82e9d77a
jiajuncao <jonnyjjc@gmail.com>
2017-02-07 18:17:50 -0500

    IB: virtualize qp num; fix ibv_create_qp().

--- contrib/infiniband/ibvidentifier.h
+++ contrib/infiniband/ibvidentifier.h
@@ -1,6 +1,7 @@
 #ifndef IBVID_H
 #define IBVID_H
 #include <stdint.h>
+#include "lib/list.h"
 
 typedef struct ibv_qp_id {
   uint32_t qpn;
@@ -17,6 +18,24 @@ struct ibv_rkey_id {
   uint32_t pd_id;
   uint32_t rkey;
 };
+
+struct ibv_rkey_pair {
+  struct ibv_rkey_id orig_rkey;
+  uint32_t new_rkey;
+  struct list_elem elem;
+};
+
+struct ibv_ud_qp_id_pair {
+  ibv_ud_qp_id_t orig_id;
+  ibv_ud_qp_id_t curr_id;
+  struct list_elem elem;
+};
+
+typedef struct qp_num_mapping {
+  uint32_t virtual_qp_num;
+  uint32_t real_qp_num;
+  struct list_elem elem;
+} qp_num_mapping_t;
 
 ibv_qp_id_t * create_ibv_id(int qpn, int lid, void * buffer, int size);
 #endif
