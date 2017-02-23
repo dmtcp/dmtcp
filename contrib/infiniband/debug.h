dec8b3c40b0950e1ad79ad3f92154908aadcdc02
jiajuncao <jonnyjjc@gmail.com>
2017-02-22 20:32:39 -0500

    IB: better debug/error handling.

--- contrib/infiniband/debug.h
+++ contrib/infiniband/debug.h
@@ -1,20 +1,36 @@
 /*
-  Debugging macroces file. Can be removed lately
-
+  Debugging macroces file.
 */
 #ifndef IBV_LIB_DEBUG_H
 #define IBV_LIB_DEBUG_H
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-//#define IBV_DEBUG
+//#define IBV_ENABLE_DEBUG
 
-#ifdef IBV_DEBUG
-#define PDEBUG(fmt, ...) \
-    do { fprintf(stderr, fmt, ## __VA_ARGS__); } while (0)
+#ifdef IBV_ENABLE_DEBUG
+#define IBV_DEBUG(args...) \
+  do { \
+    fprintf(stderr, args); \
+  } while (0)
 #else
-#define PDEBUG(fmt, ...) \
+#define IBV_DEBUG(args...) \
     do { } while (0)
 #endif
+
+#define IBV_WARNING(args...) \
+  do { \
+    fprintf(stderr, "[%d] %s:%d %s:\n", getpid(), \
+            __FILE__, __LINE__, __FUNCTION__); \
+    fprintf(stderr, args); \
+  } while (0)
+
+#define IBV_ERROR(args...) \
+  do { \
+    IBV_WARNING(args); \
+    exit(1); \
+  } while (0)
 
 #endif
