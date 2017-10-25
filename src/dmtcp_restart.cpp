a7db942e28d8365b964f44abd8897c2cd13d2d25
Rohan Garg <rohgarg@ccs.neu.edu>
2017-10-25 16:30:44 -0400

    dmtcp_restart: Fix a bug with debug logging
    Even when compiled with the logs enabled, the logs weren't getting written
    to the log file(s) in the tmp directory. The issue was that the log file
    was never opened correctly for writing.  This patch fixes this issue.

--- src/dmtcp_restart.cpp
+++ src/dmtcp_restart.cpp
@@ -211,7 +211,6 @@ class RestoreTarget
     {
       UniquePid::ThisProcess() = _pInfo.upid();
       UniquePid::ParentProcess() = _pInfo.uppid();
-      Util::initializeLogFile(_pInfo.procname());
 
       if (createIndependentRootProcesses) {
         DmtcpUniqueProcessId compId = _pInfo.compGroup().upid();
@@ -265,6 +264,7 @@ class RestoreTarget
                                &compId,
                                &coordInfo,
                                &localIPAddr);
+        Util::initializeLogFile(SharedData::getTmpDir(), _pInfo.procname());
 
         Util::prepareDlsymWrapper();
       }
