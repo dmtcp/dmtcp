dmtcp/dmtcp

commit 30c03f237900fba0d2c537af2b7aace71d9cd9e8

pid-plugin: Save and restore CPU affinities for threads

src/plugin/pid/pid.cpp
@@ -35,6 +35,7 @@ using namespace dmtcp;
 extern "C" pid_t dmtcp_update_ppid();
 
 static string pidMapFile;
+static __thread cpu_set_t threadCpuMask = {0};
 
 extern "C"
 pid_t dmtcp_real_to_virtual_pid(pid_t realPid)
@@ -205,7 +206,6 @@ static void pidVirt_RefillTid()
 }
 #endif
 
-
 extern "C" void dmtcp_event_hook(DmtcpEvent_t event, DmtcpEventData_t *data)
 {
   switch (event) {
@@ -242,6 +242,20 @@ extern "C" void dmtcp_event_hook(DmtcpEvent_t event, DmtcpEventData_t *data)
       }
       break;
 
+    case DMTCP_EVENT_PRE_SUSPEND_USER_THREAD:
+    {
+       int ret = _real_sched_getaffinity(0, sizeof(threadCpuMask), &threadCpuMask);
+       JTRACE("getaffinity returned ")(ret)(CPU_COUNT(&threadCpuMask));
+       break;
+    }
+    case DMTCP_EVENT_RESUME_USER_THREAD:
+    {
+      if (data->refillInfo.isRestart) {
+        int ret = _real_sched_setaffinity(0, sizeof(threadCpuMask), &threadCpuMask);
+        JTRACE("setaffinity returned")(ret)(CPU_COUNT(&threadCpuMask));
+      }
+      break;
+    }
     case DMTCP_EVENT_PTHREAD_RETURN:
     case DMTCP_EVENT_PTHREAD_EXIT:
       pidVirt_ThreadExit(data);

test/sched_test.c
@@ -49,7 +49,8 @@ main()
         perror("Error getting CPU affinity for child");
         //return -1;
       } else {
-        //assert(CPU_ISSET(i, &cset));
+        // Turning this off for Travis containers
+        //assert(CPU_ISSET(i, &cset) && CPU_COUNT(&cset) == 1);
         printf("CPU affinity for (%d) is (%d)\n", ret, i);
       }
       sleep(1);

commit 1d8a87997eff52f123ff2352e7ac59a067cf0743

pid: Fix an error introduced in 30c03f23
Apparently, on some old distros, the code fails to compile without this
include file.

src/plugin/pid/pid.cpp
@@ -20,6 +20,12 @@
  ****************************************************************************/
 
 #include <sys/types.h>
+
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE
+#endif // ifndef _GNU_SOURCE
+#include <sched.h>
+
 #include "jalloc.h"
 #include "jassert.h"
 #include "jconvert.h"