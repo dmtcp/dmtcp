f4ca800073b8b2e58e81f5c1ac4b8b45bc717346
Gene Cooperman <gene@ccs.neu.edu>
2019-07-21 07:39:37 -0400

    Add support for POSIX condition variables

--- src/plugin/pid/pid_mutexwrappers.cpp
+++ src/plugin/pid/pid_mutexwrappers.cpp
@@ -10,12 +10,17 @@
 
 #include "jassert.h"
 #include "pidwrappers.h"
+#include "dmtcp_dlsym.h" // for NEXT_FNC_DEFAULT macro
 
 #define __real_pthread_mutex_lock       NEXT_FNC(pthread_mutex_lock)
 #define __real_pthread_mutex_trylock    NEXT_FNC(pthread_mutex_trylock)
 #define __real_pthread_mutex_timedlock  NEXT_FNC(pthread_mutex_timedlock)
 #define __real_pthread_mutex_unlock     NEXT_FNC(pthread_mutex_unlock)
 #define __real_pthread_mutex_consistent NEXT_FNC(pthread_mutex_consistent)
+
+// For explanation why we need NEXT_FNC_DEFAULT see [1]
+#define __real_pthread_cond_wait        NEXT_FNC_DEFAULT(pthread_cond_wait)
+#define __real_pthread_cond_timedwait   NEXT_FNC_DEFAULT(pthread_cond_timedwait)
 
 // |=-------------------------------=[ GOAL ]=--------------------------------=|
 //
@@ -56,6 +61,43 @@
 //   EOWNERDEAD. If the application can't recover the mutex, it needs to call
 //   pthread_mutex_unlock(), which marks the mutex as notrecoverable
 //   (PTHREAD_MUTEX_NOTRECOVERABLE).
+//
+//
+// |=------------------------=[ CONDITION VARIABLE ]=-------------------------=|
+//
+// From 'man 3p pthread_cond_wait'
+//   pthread_cond_timedwait() and pthread_cond_wait() ... shall be called with
+//   mutex locked by the calling thread or undefined behavior results.
+//
+//   These functions atomically release mutex and cause the calling thread to
+//   block on the condition variable ...
+//
+//   Upon successful return, the mutex shall have been locked and shall be
+//   owned by the calling thread.
+//
+// Putting this words from the man page into a picture, we get the following:
+//
+//          Th1                      Th2
+//         -----                    -----
+//           |                        |
+//      lock mutex (M)                |             /// Th1 owns mutex M.
+//           |                        |
+//   wait for cond (C,M) ->           |             /// Th1 unlocks mutex M
+//           |                        |             /// when waiting for cond C.
+//           |                        |
+//           |                 lock mutex (M)       /// Th2 owns mutex M.
+//           |                        |
+//           |                  fire cond (C)
+//           |                unlock mutex (M)
+//           |                        |
+//   wait for cond (C,M) <-           |             /// Th1 owns mutex M
+//           |                        |             /// after return from
+//          ...                      ...            /// wait for cond C.
+//
+// As the pthread_cond_wait(3p)/pthread_cond_timedwait(3p) APIs don't go through
+// the public pthread_mutex_* APIs to lock the mutex once the condition variable
+// has been fired, we explicitly need to update the entry in our ownernership
+// record for the mutex which is associated with the condition variable.
 
 
 /* Globals structs */
@@ -198,3 +240,79 @@ extern "C" int pthread_mutex_unlock(pthread_mutex_t *mutex) {
 
   return rc;
 }
+
+extern "C" int pthread_cond_wait(pthread_cond_t* cond, pthread_mutex_t* mutex) {
+  int rc;
+
+  // pthread_cond_wait will unlock the mutex, but we skip unfollowing for
+  // simplicity & runtime performance and in case we checkpoint while waiting
+  // in pthread_cond_wait we build on the __owner==0 check on restore
+
+  rc = __real_pthread_cond_wait(cond, mutex);
+  if (rc == 0 && dmtcp_is_running_state()) {
+    follow_mutex(mutex);
+  }
+
+  return rc;
+}
+
+extern "C" int pthread_cond_timedwait(pthread_cond_t* cond,
+                                      pthread_mutex_t* mutex,
+                                      const struct timespec* abstime) {
+  int rc;
+
+  // pthread_cond_timedwait will unlock the mutex, but we skip unfollowing for
+  // simplicity & runtime performance and in case we checkpoint while waiting
+  // in pthread_cond_wait we build on the __owner==0 check on restore
+
+  rc = __real_pthread_cond_timedwait(cond, mutex, abstime);
+  // Even the call timed out we have the lock and own the mutex.
+  if ((rc == 0 || rc == ETIMEDOUT) && dmtcp_is_running_state()) {
+    follow_mutex(mutex);
+  }
+
+  return rc;
+}
+
+// [1] NEXT_FNC_DEFAULT for pthread_cond_wait(3p)/pthread_cond_timedwait(3p)
+// ----------------------------------------------------------------------------
+//
+// glibc provides two different implementations for these functions, one
+// for LinuxThreads and one for POSIX Threads. A dlsym lookup would return a
+// function pointer to the oldest symbol defined which in this case is the
+// implementation for LinuxThreads.
+//
+// What's behind that magic is glibc's symbol versioning.
+//   nm /lib64/libpthread.so.0 | grep pthread_cond_wait
+//     000000000000c140 T pthread_cond_wait@GLIBC_2.2.5
+//     000000000000b5c0 T pthread_cond_wait@@GLIBC_2.3.2
+// where in this case the symbol with version 2.2.5 is the LinuxThread
+// implementation and the symbol with version 2.3.2 is the POSIX Thread
+// implementation.
+//
+// A symbol that has the '@@' tag in its version information is declared as
+// the default symbol for that library. Programs that link against this library
+// will be linked against the default version of the symbol.
+// This can be seen by executing the following snippet on the cmd line:
+//   echo "
+//   #include <pthread.h>
+//   void foo() { pthread_cond_wait(0,0); }
+//   void main() {}" |
+//   gcc -O2 -o tmp.o -xc -lpthread -; nm tmp.o | grep pthread_cond_wait;
+//     rm -f tmp.o
+// which returns:
+//   U pthread_cond_wait@@GLIBC_2.3.2
+//
+// It is important that even '@@' denotes the default symbol, this is only true
+// for programs that link directly against the library, not for lookups done
+// with dlsym(3).
+// dlsym(3) makes an "unversioned" lookup for the symbol, which by default will
+// match the oldest symbol version in the library (this is because glibc
+// guarantees strong runtime backwards compatibility!).
+//
+// Since this implementation handles POSIX mutexes & condition variables we want
+// to delegate to the POSIX Thread implemenation from our wrapper functions
+// for pthread_cond_*wait(3p).
+// As these are the default version for that symbols we use the
+// NEXT_FNC_DEFAULT macro provided by dmtcp to find the next "default" symbol
+// in the link map chain.
