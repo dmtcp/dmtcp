8c20abe3d8b90c22a5145c4364fac4094d10d9cf
Gene Cooperman <gene@ccs.neu.edu>
2019-09-21 15:28:33 -0400

    Minor polishing (#777)

--- test/file2.c
+++ test/file2.c
@@ -42,7 +42,8 @@ main()
   if (mkdtemp(dir1) == NULL) {
     abort();
   }
-  memcpy(dir2, dir1, strlen(dir1));      // Update new prefix
+
+  memcpy(dir2, dir1, strlen(dir1));      // Update prefix, only, for dir2 path
   if (mkdtemp(dir2) == NULL) {
     abort();
   }
