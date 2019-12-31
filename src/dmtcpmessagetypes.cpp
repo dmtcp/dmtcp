f64dbc17d62c0bc7ab9f9cad63685487973b0201
Kapil Arya <kapil.arya.17@gmail.com>
2019-12-31 02:08:49 -0800

    Include barrier name in DmtcpMessage.
    Sometimes the process might get killed after writing DmtcpMessage but
    before writing "extra-data" on the coordinator socket. Currently, the
    coordinator is not robust enough to detect it. The fix is to send the
    barrier name as part of DmtcpMessage.
    
    Since the DmtcpMessage struct is of static size, we are imposing a
    limit of 64 characters for the barrier names.

--- src/dmtcpmessagetypes.cpp
+++ src/dmtcpmessagetypes.cpp
@@ -44,6 +44,7 @@ DmtcpMessage::DmtcpMessage(DmtcpMessageType t /*= DMT_NULL*/)
 {
   // struct sockaddr_storage _addr;
   // socklen_t _addrlen;
+  memset(&barrier, 0, sizeof(barrier));
   memset(&compGroup, 0, sizeof(compGroup));
   memset(&ipAddr, 0, sizeof ipAddr);
   memset(nsid, 0, sizeof nsid);
