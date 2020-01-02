ec2a0d5c4476f3628d007f05876e7d8ee7bc57d0
Kapil Arya <kapil.arya.17@gmail.com>
2020-01-02 00:47:09 -0800

    Removed unused barriers from event plugin.

--- src/plugin/ipc/event/eventconnlist.h
+++ src/plugin/ipc/event/eventconnlist.h
@@ -17,21 +17,11 @@ class EventConnList : public ConnectionList
 
     static void drainFd() { instance().drain(); }
 
-    static void ckpt() { instance().preCkpt(); }
-
     static void resumeRefill() { instance().refill(false); }
-
-    static void resumeResume() { instance().resume(false); }
 
     static void restart() { instance().postRestart(); }
 
-    static void restartRegisterNSData() { instance().registerNSData(); }
-
-    static void restartSendQueries() { instance().sendQueries(); }
-
     static void restartRefill() { instance().refill(true); }
-
-    static void restartResume() { instance().resume(true); }
 
     virtual int protectedFd() { return PROTECTED_EVENT_FDREWIRER_FD; }
 
