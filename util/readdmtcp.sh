4706c7787a34b805f57ea03221ee0addad45ce71
Gene Cooperman <gc00@github.org>
2023-08-24 15:19:48 -0400

    readdmtcp.sh: Prefer mtcp_restart of same dir tree

--- util/readdmtcp.sh
+++ util/readdmtcp.sh
@@ -16,13 +16,13 @@ fi
 
 dir=`dirname $0`
 
-if which mtcp_restart > /dev/null 2> /dev/null; then
-  mtcp_restart --simulate $1 2>&1
-  exit 0
-fi
-
 # This next one assumes that this script resides in DMTCP_ROOT/util/
 if test -x $dir/../bin/mtcp_restart; then
   $dir/../bin/mtcp_restart --simulate $1 2>&1
+  exit 0
+fi
+
+if which mtcp_restart > /dev/null 2> /dev/null; then
+  mtcp_restart --simulate $1 2>&1
   exit 0
 fi
