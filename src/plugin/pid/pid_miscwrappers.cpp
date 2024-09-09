3339616a7c43832cc458938d89f11dcf76b1f1f8
Gene Cooperman <gene@ccs.neu.edu>
2024-09-09 17:38:25 -0400

    ARM64 comment:__NR_getpgrp not found in glibc-2.36

--- src/plugin/pid/pid_miscwrappers.cpp
+++ src/plugin/pid/pid_miscwrappers.cpp
@@ -347,6 +347,7 @@ syscall(long sys_num, ...)
 // SYS_getpgrp undefined in aarch64.
 // Presumably, it's handled by libc, and is not a kernel call
 //   in AARCH64 (e.g., v5.01).
+// Tested with glibc-2.36:  __NR_getpgrp not defined on ARM64.
 #ifndef __aarch64__
   case SYS_getpgrp:
   {
