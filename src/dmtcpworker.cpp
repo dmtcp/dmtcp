dmtcp/dmtcp

commit 7e486722bdb288b40bf29da47d204c639d9fa6bf

Save proc maps in coordinator db.

src/dmtcpworker.cpp
@@ -28,8 +28,10 @@
 #include "../jalib/jfilesystem.h"
 #include "../jalib/jsocket.h"
 #include "coordinatorapi.h"
+#include "kvdb.h"
 #include "pluginmanager.h"
 #include "processinfo.h"
+#include "procselfmaps.h"
 #include "shareddata.h"
 #include "syscallwrappers.h"
 #include "syslogwrappers.h"
@@ -42,6 +44,7 @@ using namespace dmtcp;
 LIB_PRIVATE void dmtcp_prepare_atfork(void);
 
 EXTERNC void *ibv_get_device_list(void *) __attribute__((weak));
+EXTERNC ProcSelfMaps *procSelfMaps;
 
 /* The following instance of the DmtcpWorker is just to trigger the constructor
  * to allow us to hijack the process
@@ -438,12 +441,20 @@ DmtcpWorker::preCheckpoint()
 void
 DmtcpWorker::postCheckpoint()
 {
+  {
+    // Send ckpt maps to coordinator.
+    string workerPath("/worker/" + ProcessInfo::instance().upidStr());
+    kvdb::set(
+      workerPath,
+      "ProcSelfMaps_Ckpt",
+      procSelfMaps->getData());
+  }
+
   WorkerState::setCurrentState(WorkerState::CHECKPOINTED);
 
   // TODO: Merge this barrier with the previous `sendCkptFilename` msg.
   JTRACE("Waiting for Write-Ckpt barrier");
   CoordinatorAPI::waitForBarrier("DMT:WriteCkpt");
-
 
   /* Now that temp checkpoint file is complete, rename it over old permanent
    * checkpoint file.  Uses rename() syscall, which doesn't change i-nodes.
@@ -451,7 +462,6 @@ DmtcpWorker::postCheckpoint()
    */
   JASSERT(rename(ProcessInfo::instance().getTempCkptFilename().c_str(),
                  ProcessInfo::instance().getCkptFilename().c_str()) == 0);
-
 
   CoordinatorAPI::sendCkptFilename();
 
@@ -480,6 +490,16 @@ DmtcpWorker::postRestart(double ckptReadTime)
   PluginManager::eventHook(DMTCP_EVENT_RESTART);
 
   JTRACE("got resume message after restart");
+
+  {
+    // Send ckpt maps to coordinator.
+    string workerPath("/worker/" + ProcessInfo::instance().upidStr());
+    ProcSelfMaps procSelfMaps;
+    kvdb::set(
+      workerPath,
+      "ProcSelfMaps_Rst",
+      procSelfMaps.getData());
+  }
 
   // Inform Coordinator of RUNNING state.
   WorkerState::setCurrentState(WorkerState::RUNNING);

include/procselfmaps.h
@@ -26,6 +26,8 @@ class ProcSelfMaps
 
     int getNextArea(ProcMapsArea *area);
 
+    const char* getData() const { return data; }
+
   private:
     unsigned long int readDec();
     unsigned long int readHex();

src/writeckpt.cpp
@@ -340,10 +340,6 @@ mtcp_writememoryareas(int fd)
     }
   }
 
-  // Release the memory.
-  delete procSelfMaps;
-  procSelfMaps = NULL;
-
   /* It's now safe to do this, since we're done using writememoryarea() */
   remap_nscd_areas(*nscdAreas);
 

commit 6208b1ee7bd876358bcda967efd103a831fbec3e

Fix for forked ckpt: commit 7e486722bdb broke it

configure
@@ -1525,7 +1525,8 @@ Optional Features:
   --enable-forked-checkpointing
                           fork a child process to do checkpointing, so that
                           parent sees only minor delay during checkpoint.
-                          (EXPERIMENTAL)
+                          (EXPERIMENTAL: Currently, shared memory is not
+                          supported.)
   --enable-fast-restart   uses tricks to mmap from checkpoint image file;
                           disables all kinds of compression (EXPERIMENTAL)
   --disable-test-suite    disables "make check"; target apps for testing (e.g.

configure.ac
@@ -155,7 +155,8 @@ AC_ARG_ENABLE([forked_checkpointing],
             [AS_HELP_STRING([--enable-forked-checkpointing],
                             [fork a child process to do checkpointing, so that
                             parent sees only minor delay during checkpoint.
-			    (EXPERIMENTAL)])],
+			    (EXPERIMENTAL: Currently, shared memory is not
+                             supported.)])],
             [use_forked_ckpt=$enableval],
             [use_forked_ckpt=no])
 

src/dmtcpworker.cpp
@@ -475,12 +475,14 @@ DmtcpWorker::postCheckpoint()
     kvdb::set(workerPath, "ProcSelfMaps_JAllocArenas", o.str());
   }
 
+#ifndef FORKED_CHECKPOINTING
   {
     kvdb::set(
       workerPath,
       "ProcSelfMaps_Ckpt",
       procSelfMaps->getData());
   }
+#endif
 
   WorkerState::setCurrentState(WorkerState::CHECKPOINTED);
 