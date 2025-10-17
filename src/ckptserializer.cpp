b15389ae55aa764b282cab7952fcad1752480c43
Gene Cooperman <gene@ccs.neu.edu>
2025-10-17 01:49:05 -0400

    Use double_fork() for gzip compression
     * This fixes a bug, when using forked checkpointing.
       Previously, we forked a gzip, and did a waitpid for it.
       So, the parent process was stuck in waitpid while the
         forked process writing to a ckpt image.

--- src/ckptserializer.cpp
+++ src/ckptserializer.cpp
@@ -59,49 +59,34 @@
 
 using namespace dmtcp;
 
-#define FORKED_CKPT_FAILED 0
-#define FORKED_CKPT_PARENT 1
-#define FORKED_CKPT_CHILD  2
+typedef enum {
+  FORKED_CKPT_FAILED,
+  FORKED_CKPT_PARENT,
+  FORKED_CKPT_GRANDCHILD
+} forked_rc_t;
 
-static int forked_ckpt_status = -1;
-static pid_t ckpt_extcomp_child_pid = -1;
-static struct sigaction saved_sigchld_action;
+static forked_rc_t forked_ckpt_status = FORKED_CKPT_FAILED;
 static int open_ckpt_to_write(int fd, int pipe_fds[2], char **extcomp_args);
 void mtcp_writememoryareas(int fd) __attribute__((weak));
 
-/* We handle SIGCHLD while checkpointing. */
 static void
-default_sigchld_handler(int sig)
+trivial_sigchld_handler(int sig)
 {
   JASSERT(sig == SIGCHLD);
 }
-
 static void
-prepare_sigchld_handler()
+set_trivial_sigchld_action(struct sigaction *trivial_sigchld_action_ptr)
 {
-  /* 3a. Set SIGCHLD to our own handler;
-   *     User handling is restored after gzip finishes.
-   * SEE: sigaction(2), NOTES: only portable method of avoiding zombies
-   *      is to catch SIGCHLD signal and perform a wait(2) or similar.
-   *
-   * NOTE: Although the default action for SIGCHLD is supposedly SIG_IGN,
-   * for historical reasons there are differences between SIG_DFL and SIG_IGN
-   * for SIGCHLD.  See sigaction(2), NOTES section for more details. */
-  struct sigaction default_sigchld_action;
-
-  memset(&default_sigchld_action, 0, sizeof(default_sigchld_action));
-  default_sigchld_action.sa_handler = default_sigchld_handler;
-  sigaction(SIGCHLD, &default_sigchld_action, &saved_sigchld_action);
+  memset(trivial_sigchld_action_ptr, 0, sizeof(*trivial_sigchld_action_ptr));
+  trivial_sigchld_action_ptr->sa_handler = trivial_sigchld_handler;
 }
-
 static void
-restore_sigchld_handler_and_wait_for_zombie(pid_t pid)
+allow_delivery_of_one_sigchld()
 {
   /* This is done to avoid calling the user SIGCHLD handler when gzip
    * or another compression utility exits.
-   * NOTE: We must wait in case user did rapid ckpt-kill in succession.
-   *  Otherwise, kernel could have optimization allowing us to close fd and
-   *  rename tmp ckpt file to permanent even while gzip is still writing.
+   * NOTE: rename tmp ckpt file is done in dmtcpworker.cpp.  If user did rapid
+   *       ckpt-kill in succession, then rename doesn't happen. So, no problem.
    */
 
   /* All signals, incl. SIGCHLD, were blocked in
@@ -109,16 +94,52 @@ restore_sigchld_handler_and_wait_for_zombie(pid_t pid)
    * the gzip child process was forked.  The child may have already
    * terminated and returned, creating a blocked SIGCHLD.
    *   So, we use sigsuspend so that we can clean up the child zombie
-   * with wait4 whether it already terminated or not yet.
+   * with waitpid/wait4 whether it already terminated or not yet.
    */
-  sigset_t suspend_sigset;
+  sigset_t mask_all_but_sigchld;
+  sigfillset(&mask_all_but_sigchld);
+  sigdelset(&mask_all_but_sigchld, SIGCHLD);
+  _real_sigsuspend(&mask_all_but_sigchld); // block everything except SIGCHLD
+  // This assumes SIGCHLD handler is trivial_sigchld_action, not SIG_IGN.
+  // sigsuspend was interrupted when child process called _exit: SIGCHLD
+  // delivered, and mask_all_but_sigchld is replaced by the old mask, which
+  //   then bloks SIGCHLD: So, any second SIGSHLD will be blocked.
+}
 
-  sigfillset(&suspend_sigset);
-  sigdelset(&suspend_sigset, SIGCHLD);
-  _real_sigsuspend(&suspend_sigset);
-  JWARNING(_real_waitpid(pid, NULL, 0) != -1) (pid) (JASSERT_ERRNO);
-  pid = -1;
-  sigaction(SIGCHLD, &saved_sigchld_action, NULL);
+// returns FORKED_PARENT, FORKED_GRANDCHILD, FORKED_CKPT_FAILED
+static forked_rc_t
+double_fork()
+{
+  pid_t pid;
+  // 0. This assumes SIGCHLD is already blocked.  This works during checkpoint.
+  struct sigaction saved_sigchld_action;
+  // 1. Set SIGCHLD handler to trivial handler; save original SIGCHLD handler
+  // NOTE: allow_delivery_of_one_sigchld calls sigsuspend(). This is unblocked
+  //       when SIGCHLD arrives, only for trivial_sigchld_action, not SIG_IGN.
+  struct sigaction trivial_sigchld_action;
+  set_trivial_sigchld_action(&trivial_sigchld_action);
+  sigaction(SIGCHLD, &trivial_sigchld_action, &saved_sigchld_action);
+  pid = _real_sys_fork();
+  JASSERT(pid >= 0)(pid); // Or could do:  return FORKED_CKPT_FAILED;
+
+  if (pid > 0) {
+    // 2. Allow delivery of just one SIGCHLD (due to the child that did _exit())
+    allow_delivery_of_one_sigchld();
+
+    // 3. Next, we reap that child process (which is now a zombie).
+    JWARNING(_real_waitpid(pid, NULL, 0) != -1) (pid) (JASSERT_ERRNO);
+
+    // 4. ... and we then restore the original SIGCHLD handler
+    sigaction(SIGCHLD, &saved_sigchld_action, NULL);
+    return FORKED_CKPT_PARENT;
+  } else {
+    pid = _real_sys_fork();
+    JASSERT(pid >= 0)(pid);
+    if (pid > 0) {
+      _exit(EXIT_SUCCESS); // Child process exits, delivers SIGCHLD
+    }
+    return FORKED_CKPT_GRANDCHILD;
+  }
 }
 
 /*
@@ -225,19 +246,14 @@ perform_open_ckpt_image_fd(const char *tempCkptFilename,
   */
 
   if (use_deltacompression || use_gzip_compression) { /* fork compr. process */
-    /* 3a. Set SIGCHLD to our own handler;
-     *     User handling is restored after gzip finishes.
-     */
-    prepare_sigchld_handler();
-
-    /* 3b. Open pipe */
+    /* 3a. Open pipe */
     int pipe_fds[2];
     if (_real_pipe(pipe_fds) == -1) {
       JWARNING(false).Text("Error creating pipe. Compression won't be used.");
       use_gzip_compression = use_deltacompression = 0;
     }
 
-    /* 3c. Fork compressor child */
+    /* 3b. Fork compressor child */
     if (use_gzip_compression) {/* fork a gzip process */
       *use_compression = true;
       fd = open_ckpt_to_write_gz(fd, pipe_fds, gzip_path);
@@ -253,21 +269,24 @@ perform_open_ckpt_image_fd(const char *tempCkptFilename,
   return fd;
 }
 
-static int
+static forked_rc_t
 test_and_prepare_for_forked_ckpt()
 {
 #ifdef TEST_FORKED_CHECKPOINTING
-  return 1;
+  return FORKED_CKPT_PARENT;
 #endif  // ifdef TEST_FORKED_CHECKPOINTING
 
   if (getenv(ENV_VAR_FORKED_CKPT) == NULL) {
-    return 0;
+    return FORKED_CKPT_FAILED;
   }
 
   /* Set SIGCHLD to our own handler;
    *     User handling is restored after forking child process.
    */
-  prepare_sigchld_handler();
+  struct sigaction trivial_sigchld_action;
+  struct sigaction saved_sigchld_action;
+  set_trivial_sigchld_action(&trivial_sigchld_action);
+  sigaction(SIGCHLD, &trivial_sigchld_action, &saved_sigchld_action);
 
   pid_t forked_cpid = _real_sys_fork();
   if (forked_cpid == -1) {
@@ -275,7 +294,9 @@ test_and_prepare_for_forked_ckpt()
     .Text("Failed to do forked checkpointing, trying normal checkpoint");
     return FORKED_CKPT_FAILED;
   } else if (forked_cpid > 0) {
-    restore_sigchld_handler_and_wait_for_zombie(forked_cpid);
+    JWARNING(_real_waitpid(forked_cpid, NULL, 0) != -1)
+            (forked_cpid) (JASSERT_ERRNO);
+    sigaction(SIGCHLD, &saved_sigchld_action, NULL);
     JTRACE("checkpoint complete\n");
     return FORKED_CKPT_PARENT;
   } else {
@@ -291,31 +312,29 @@ test_and_prepare_for_forked_ckpt()
     /* grandchild continues; no need now to waitpid() on grandchild */
     JTRACE("inside grandchild process");
   }
-  return FORKED_CKPT_CHILD;
+  return FORKED_CKPT_GRANDCHILD;
 }
 
 int
 open_ckpt_to_write(int fd, int pipe_fds[2], char **extcomp_args)
 {
-  pid_t cpid;
-
-  cpid = _real_sys_fork();
-  if (cpid == -1) {
+  forked_rc_t fork_rc = double_fork();
+  if (fork_rc == FORKED_CKPT_FAILED) {
     JWARNING(false) (extcomp_args[0]) (JASSERT_ERRNO)
     .Text("WARNING: error forking child process. Compression won't be used");
     _real_close(pipe_fds[0]);
     _real_close(pipe_fds[1]);
     pipe_fds[0] = pipe_fds[1] = -1;
 
-    // fall through to return fd
-  } else if (cpid > 0) { /* parent process */
+  // fall through to return fd
+  } else if (fork_rc == FORKED_CKPT_PARENT) { /* parent process */
     // Before running gzip in child process, we must not use LD_PRELOAD.
     // See revision log 342 for details concerning bash.
-    ckpt_extcomp_child_pid = cpid;
     JWARNING(_real_close(pipe_fds[0]) == 0) (JASSERT_ERRNO)
     .Text("WARNING: close failed");
     fd = pipe_fds[1];  // change return value
   } else { /* child process */
+    JASSERT(fork_rc == FORKED_CKPT_GRANDCHILD)(fork_rc);
     // static int (*libc_unsetenv) (const char *name);
     // static int (*libc_execvp) (const char *path, char *const argv[]);
 
@@ -409,11 +428,6 @@ CkptSerializer::writeCkptImage(DmtcpCkptHeader ckptHdr,
   mtcp_writememoryareas(fd);
 
   if (use_compression) {
-    /* In perform_open_ckpt_image_fd(), we set SIGCHLD to our own handler.
-     * Restore it now.
-     */
-    restore_sigchld_handler_and_wait_for_zombie(ckpt_extcomp_child_pid);
-
     /* IF OUT OF DISK SPACE, REPORT IT HERE. */
     JASSERT(fsync(fdCkptFileOnDisk) != -1) (JASSERT_ERRNO)
     .Text("(compression): fsync error on checkpoint file");
@@ -421,7 +435,7 @@ CkptSerializer::writeCkptImage(DmtcpCkptHeader ckptHdr,
     .Text("(compression): error closing checkpoint file.");
   }
 
-  if (forked_ckpt_status == FORKED_CKPT_CHILD) {
+  if (forked_ckpt_status == FORKED_CKPT_GRANDCHILD) {
     /* Now that temp checkpoint file is complete, rename it over old permanent
      * checkpoint file.  Uses rename() syscall, which doesn't change i-nodes.
      * So, gzip process can continue to write to file even after renaming.
