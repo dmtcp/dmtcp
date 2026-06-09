42b19c1ae05560a06fe3840fb52fbe1c83520a7f
Kapil Arya <kapila@nvidia.com>
2026-06-08 23:25:55 -0700

    refactor: simplify DMTCP allocator for C++20

--- include/dmtcpalloc.h
+++ include/dmtcpalloc.h
@@ -25,14 +25,16 @@
 #include <cstddef>
 #include <fstream>
 #include <iostream>
-#include <limits>
 #include <list>
+#include <limits>
 #include <map>
+#include <new>
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
 #include <sstream>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -44,216 +46,97 @@ template<typename T>
 class DmtcpAlloc
 {
   public:
-    typedef size_t size_type;
-    typedef ptrdiff_t difference_type;
-    typedef T *pointer;
-    typedef const T *const_pointer;
-    typedef T&reference;
-    typedef const T&const_reference;
-    typedef T value_type;
-#if __cplusplus >= 201103L
-    typedef std::true_type propagate_on_container_move_assignment;
-#endif // if __cplusplus >= 201103L
+    using value_type = T;
+    using propagate_on_container_move_assignment = std::true_type;
 
   public:
     // Constructors
-    DmtcpAlloc() throw() {}
+    // This allocator is stateless; defaulted special members have no custom
+    // behavior.
+    DmtcpAlloc() noexcept = default;
 
-    DmtcpAlloc(const DmtcpAlloc&) throw() {}
+    DmtcpAlloc(const DmtcpAlloc&) noexcept = default;
 
     template<typename U>
-    DmtcpAlloc(const DmtcpAlloc<U>&) throw() {}
+    DmtcpAlloc(const DmtcpAlloc<U>&) noexcept {}
 
-    DmtcpAlloc&operator=(const DmtcpAlloc&)
-    {
-      return *this;
-    }
+    DmtcpAlloc& operator=(const DmtcpAlloc&) noexcept = default;
 
-    // Destructor
-    ~DmtcpAlloc() throw()
-    {}
-
-    // Utility functions
-    pointer address(reference r) const
-    {
-      return &r;
-    }
-
-    const_pointer address(const_reference c) const
-    {
-      return &c;
-    }
-
-    size_type max_size() const
-    {
-      return std::numeric_limits<size_t>::max() / sizeof(T);
-    }
-
-#if __cplusplus >= 201103L
-
-    // In-place construction
-    template<typename X, typename ... Args>
-    void construct(X *p, Args&& ... args)
-    {
-      ::new ((void *)p)X(std::forward<Args>(args) ...);
-    }
-
-    // In-place destruction
-    template<typename X>
-    void destroy(X *p)
-    {
-      p->~X();
-    }
-
-#else // if __cplusplus >= 201103L
-
-    // In-place construction
-    void construct(pointer p, const_reference c)
-    {
-      // placement new operator
-      new (reinterpret_cast<void *>(p))T(c);
-    }
-
-    // In-place construction
-    template<typename X>
-    void construct(X *p, const X &c)
-    {
-      // placement new operator
-      new (reinterpret_cast<void *>(p))X(c);
-    }
-
-    // In-place destruction
-    void destroy(pointer p)
-    {
-      // call destructor directly
-      (p)->~T();
-    }
-
-# if __GNUC__ == 4 && __GNUC_MINOR__ == 0
-
-    // In-place destruction
-    // This is a workaround for a bug in gcc-4.0.
-    template<typename X>
-    void destroy(X *p)
-    {
-      // call destructor directly
-      (p)->~X();
-    }
-# endif // if __GNUC__ == 4 && __GNUC_MINOR__ == 0
-#endif // if __cplusplus >= 201103L
-
-    // Rebind to allocators of other types
-    template<typename U>
-    struct rebind {
-      typedef DmtcpAlloc<U>other;
-    };
+    ~DmtcpAlloc() noexcept = default;
 
     // Allocate raw memory
-    pointer allocate(size_type n, const void * = NULL)
+    T *allocate(std::size_t n)
     {
-      // void* p = malloc( n * sizeof(T) );
-      // if( p == NULL )
-      // throw std::bad_alloc();
-      // return pointer(p);
-      return pointer(jalib::JAllocDispatcher::malloc(n * sizeof(T)));
+      if (n > std::numeric_limits<std::size_t>::max() / sizeof(T)) {
+        throw std::bad_array_new_length();
+      }
+      return static_cast<T *>(jalib::JAllocDispatcher::malloc(n * sizeof(T)));
     }
 
     // Free raw memory.
-    // Note that C++ standard defines this function as
-    // deallocate( pointer p, size_type). Because Visual C++ 6.0
-    // compiler doesn't support template rebind, Dinkumware uses
-    // void* hack.
-    void deallocate(void *p, size_type n)
+    void deallocate(T *p, std::size_t)
     {
-      //// assert( p != NULL );
-      //// The standard states that p must not be NULL. However, some
-      //// STL implementations fail this requirement, so the check must
-      //// be made here.
-      // if( p == NULL )
-      // return;
-      // free( p );
-      jalib::JAllocDispatcher::free(p); //, n * sizeof(T));
+      jalib::JAllocDispatcher::free(p);
     }
-
-    //// Non-standard Dinkumware hack for Visual C++ 6.0 compiler.
-    //// VC 6 doesn't support template rebind.
-    // char* _Charalloc( size_type nBytes )
-    // {
-    // char* p = reinterpret_cast<char*>( malloc( nBytes ) );
-    // if( p == NULL )
-    // throw dmtcp::bad_alloc();
-    // return p;
-    // }
 }; // end of DmtcpAlloc
 
 // Comparison
 template<typename T1, typename T2>
 bool
-operator==(const DmtcpAlloc<T1>&, const DmtcpAlloc<T2>&) throw()
+operator==(const DmtcpAlloc<T1>&, const DmtcpAlloc<T2>&) noexcept
 {
   return true;
 }
 
 template<typename T1, typename T2>
 bool
-operator!=(const DmtcpAlloc<T1>&, const DmtcpAlloc<T2>&) throw()
+operator!=(const DmtcpAlloc<T1>&, const DmtcpAlloc<T2>&) noexcept
 {
   return false;
 }
 
-typedef std::basic_string<char, std::char_traits<char>,
-                          DmtcpAlloc<char> >string;
-typedef std::basic_stringstream<char, std::char_traits<char>,
-                                DmtcpAlloc<char> >stringstream;
-typedef std::basic_istringstream<char, std::char_traits<char>,
-                                 DmtcpAlloc<char> >istringstream;
-typedef std::basic_ostringstream<char, std::char_traits<char>,
-                                 DmtcpAlloc<char> >ostringstream;
-typedef std::ostream ostream;
-typedef std::istream istream;
-typedef std::iostream iostream;
-typedef std::fstream fstream;
-typedef std::ofstream ofstream;
-typedef std::ifstream ifstream;
+using string =
+  std::basic_string<char, std::char_traits<char>, DmtcpAlloc<char>>;
+using stringstream =
+  std::basic_stringstream<char, std::char_traits<char>, DmtcpAlloc<char>>;
+using istringstream =
+  std::basic_istringstream<char, std::char_traits<char>, DmtcpAlloc<char>>;
+using ostringstream =
+  std::basic_ostringstream<char, std::char_traits<char>, DmtcpAlloc<char>>;
+using ostream = std::ostream;
+using istream = std::istream;
+using iostream = std::iostream;
+using fstream = std::fstream;
+using ofstream = std::ofstream;
+using ifstream = std::ifstream;
 
 template<typename T>
-class vector : public std::vector<T, DmtcpAlloc<T> >
-{
-  public:
-    vector(size_t n, const T &v = T()) : std::vector<T, DmtcpAlloc<T> >(n, v) {}
-
-    vector() : std::vector<T, DmtcpAlloc<T> >() {}
-};
+using vector = std::vector<T, DmtcpAlloc<T>>;
 
 template<typename T>
-class list : public std::list<T, DmtcpAlloc<T> >
-{};
+using list = std::list<T, DmtcpAlloc<T>>;
 
 template<typename K, typename V>
-class map : public std::map<K,
-                            V,
-                            std::less<K>,
-                            DmtcpAlloc<std::pair<const K, V>>>
-{};
+using map = std::map<K,
+                     V,
+                     std::less<K>,
+                     DmtcpAlloc<std::pair<const K, V>>>;
 
 template<typename K, typename V>
-class unordered_map : public std::unordered_map<K,
-                            V,
-                            std::hash<K>,
-                            std::equal_to<K>,
-                            DmtcpAlloc<std::pair<const K, V>>>
-{};
+using unordered_map = std::unordered_map<K,
+                                         V,
+                                         std::hash<K>,
+                                         std::equal_to<K>,
+                                         DmtcpAlloc<std::pair<const K, V>>>;
 
 template<typename K>
-class set : public std::set<K, std::less<K>, DmtcpAlloc<K> >
-{};
+using set = std::set<K, std::less<K>, DmtcpAlloc<K>>;
 
 template<typename K>
-class unordered_set : public std::unordered_set<K,
-                                                std::hash<K>,
-                                                std::equal_to<K>,
-                                                DmtcpAlloc<K> >
-{};
+using unordered_set = std::unordered_set<K,
+                                         std::hash<K>,
+                                         std::equal_to<K>,
+                                         DmtcpAlloc<K>>;
 }
 
 // custom specialization of std::hash can be injected in namespace std
