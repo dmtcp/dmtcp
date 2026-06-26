11baf8b98e0658e39c37f49e6d1041e785c6d52d
Gene Cooperman <gene@ccs.neu.edu>
2026-06-25 23:12:46 -0400

    close_range() is not defined prior to glibc-2.34

--- src/wrappers.cpp
+++ src/wrappers.cpp
@@ -376,6 +376,7 @@ close(int fd)
   return ret;
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) && __GLIBC_PREREQ(2, 34)
 extern "C" int
 close_range(unsigned int first, unsigned int last, int flags)
 {
@@ -409,6 +410,7 @@ close_range(unsigned int first, unsigned int last, int flags)
 
   return ret;
 }
+#endif
 
 extern "C" int
 fclose(FILE *fp)
